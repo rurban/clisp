14beeaa655efc00e5b8a46b3753037942d1674fd
Bruno Haible <bruno@clisp.org>
1998-08-09 21:38:39 +0000

    1998-08-09-copyright.diff

--- src/spvw.d
+++ src/spvw.d
@@ -14831,7 +14831,7 @@ local uintC generation;
 
 static const char * const copyright_notice[] = {
   "                                                                    ",
-  "Copyright (c) Bruno Haible, Michael Stoll, Marcus Daniels 1992-1997 ",
+  "Copyright (c) Bruno Haible, Michael Stoll, Marcus Daniels 1992-1998 ",
   "                                                                     ",
   "This program is free software; you can redistribute it and/or modify",
   "it under the terms of the GNU General Public License as published by",
