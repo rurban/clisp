879aba2cdd876238c49c28d7942ca4d7d151ba3b
Bruno Haible <bruno@clisp.org>
1998-08-09 22:29:46 +0000

    Regenerated.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Generated automatically from src/VERSION. */
-#define VERSION "1998-08-05"
+#define VERSION "1998-08-09"
 #define VERSION_YYYY 1998
 #define VERSION_YYYY_STRING "1998"
 #define VERSION_MM 8
-#define VERSION_DD 5
+#define VERSION_DD 9
