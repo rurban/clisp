97e15d8e581f4970b77616c657bb029c4f6180a5
haible <bruno@clisp.org>
1998-08-09 20:11:47 +0000

    Prepare for release.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Generated automatically from src/VERSION. */
-#define VERSION "1998-07-30"
+#define VERSION "1998-08-05"
 #define VERSION_YYYY 1998
 #define VERSION_YYYY_STRING "1998"
-#define VERSION_MM 7
-#define VERSION_DD 30
+#define VERSION_MM 8
+#define VERSION_DD 5
