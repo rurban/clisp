91e6470d087d0bea77b5a45ff301cf49a931368c
haible <bruno@clisp.org>
1998-08-29 21:59:50 +0000

    1998-07-26j-spvw_sig.diff

---
+++ src/spvw_sigsegv.d
@@ -0,0 +1,86 @@
+# Handling of signal SIGSEGV (or SIGBUS on some platforms).
+
+# ------------------------------ Specification ---------------------------------
+
+#if defined(SELFMADE_MMAP) || defined(GENERATIONAL_GC)
+
+# Install the signal handler for catching page faults.
+  local void install_segv_handler (void);
+
+#endif # SELFMADE_MMAP || GENERATIONAL_GC
+
+#ifdef NOCOST_SP_CHECK
+
+# Install the stack overflow handler.
+# install_stackoverflow_handler(size);
+# > size: size of substitute stack.
+# This function must be called from main(); it allocates the substitute stack
+# using alloca().
+  local void install_stackoverflow_handler (uintL size);
+
+#endif
+
+# ------------------------------ Implementation --------------------------------
+
+#if defined(SELFMADE_MMAP) || defined(GENERATIONAL_GC)
+
+  # Put a breakpoint here if you want to catch CLISP just before it dies.
+  global void sigsegv_handler_failed(address)
+    var void* address;
+    { asciz_out_1(DEUTSCH ? NLstring "SIGSEGV kann nicht behoben werden. Fehler-Adresse = 0x%x." NLstring :
+                  ENGLISH ? NLstring "SIGSEGV cannot be cured. Fault address = 0x%x." NLstring :
+                  FRANCAIS ? NLstring "SIGSEGV ne peut être relevé. Adresse fautive = 0x%x." NLstring :
+                  "",
+                  address
+                 );
+    }
+
+  # Signal-Handler für Signal SIGSEGV u.ä.:
+  local int sigsegv_handler (void* fault_address)
+    { set_break_sem_0();
+      switch (handle_fault((aint)fault_address))
+        { case handler_done:
+            # erfolgreich
+            clr_break_sem_0();
+            return 1;
+          case handler_failed:
+            # erfolglos
+            sigsegv_handler_failed(fault_address);
+            # Der Default-Handler wird uns in den Debugger führen.
+          default:
+            clr_break_sem_0();
+            return 0;
+        }
+    }
+
+  # Alle Signal-Handler installieren:
+  local void install_segv_handler (void);
+  local void install_segv_handler()
+    { sigsegv_install_handler(&sigsegv_handler); }
+
+#endif # SELFMADE_MMAP || GENERATIONAL_GC
+
+#ifdef NOCOST_SP_CHECK
+
+  local void stackoverflow_handler (int emergency);
+  local void stackoverflow_handler(emergency)
+    var int emergency;
+    { if (emergency)
+        { asciz_out(DEUTSCH ? "Szenario Apollo 13: Stack-Überlauf-Behandlung ging schief. Beim nächsten Stack-Überlauf kracht's!!!" NLstring :
+                    ENGLISH ? "Apollo 13 scenario: Stack overflow handling failed. On the next stack overflow we will crash!!!" NLstring :
+                    FRANCAIS ? "Scénario Apollo 13 : Réparation de débordement de pile a échoué. Au prochain débordement de pile, ça cassera!!!" NLstring :
+                    ""
+                   );
+        }
+      SP_ueber();
+    }
+
+  # Must allocate room for a substitute stack for the stack overflow
+  # handler itself. This cannot be somewhere in the regular stack,
+  # because we want to unwind the stack in case of stack overflow.
+  #define install_stackoverflow_handler(size)  \
+    { var void* room = alloca(size);                                          \
+      stackoverflow_install_handler(&stackoverflow_handler,(void*)room,size); \
+    }
+
+#endif
---
+++ src/spvw_sigwinch.d
@@ -0,0 +1,97 @@
+# Handling of signal SIGWINCH.
+
+# ------------------------------ Specification ---------------------------------
+
+#if defined(HAVE_SIGNALS) && defined(SIGWINCH) && !defined(NO_ASYNC_INTERRUPTS)
+# Install a handler for SIGWINCH.
+  local void install_sigwinch_handler (void);
+#endif
+
+#ifdef HAVE_SIGNALS
+# Get the window width and adapt SYS::*PRIN-LINELENGTH* to it.
+  local void update_linelength (void);
+#endif
+
+# Block signal SIGWINCH during GC.
+# gc_signalblock_on(); ... gc_signalblock_off();
+
+# ------------------------------ Implementation --------------------------------
+
+#ifdef HAVE_SIGNALS
+
+# Paßt den Wert von SYS::*PRIN-LINELENGTH* an die aktuelle Breite des
+# Terminal-Fensters an.
+# update_linelength();
+  local void update_linelength (void);
+  local void update_linelength()
+    { # SYS::*PRIN-LINELENGTH* := Breite des Terminal-Fensters - 1
+      #if !defined(NEXTAPP)
+      # [vgl. 'term.c' in 'calc' von Hans-J. Böhm, Vernon Lee, Alan J. Demers]
+      if (isatty(stdout_handle)) # Standard-Output ein Terminal?
+        { /* var int lines = 0; */
+          var int columns = 0;
+          #ifdef TIOCGWINSZ
+          # Probiere erst ioctl:
+          { var struct winsize stdout_window_size;
+            if (!( ioctl(stdout_handle,TIOCGWINSZ,&stdout_window_size) <0))
+              { /* lines = stdout_window_size.ws_row; */
+                columns = stdout_window_size.ws_col;
+          }   }
+          # Das kann - entgegen der Dokumentation - scheitern!
+          if (/* (lines > 0) && */ (columns > 0)) goto OK;
+          #endif
+          #ifndef WATCOM
+          # Nun probieren wir's über termcap:
+          { var char* term_name = getenv("TERM");
+            if (term_name==NULL) { term_name = "unknown"; }
+           {var char termcap_entry_buf[10000];
+            if ( tgetent(&!termcap_entry_buf,term_name) ==1)
+              { /* lines = tgetnum("li"); if (lines<0) { lines = 0; } */
+                columns = tgetnum("co"); if (columns<0) { columns = 0; }
+              }
+          }}
+          #endif
+          # Hoffentlich enthält columns jetzt einen vernünftigen Wert.
+          if (/* (lines > 0) && */ (columns > 0)) goto OK;
+          if (FALSE)
+            { OK:
+              # Wert von SYS::*PRIN-LINELENGTH* verändern:
+              Symbol_value(S(prin_linelength)) = fixnum(columns-1);
+            }
+        }
+      #else # defined(NEXTAPP)
+      if (nxterminal_line_length > 0)
+        # Wert von SYS::*PRIN-LINELENGTH* verändern:
+        { Symbol_value(S(prin_linelength)) = fixnum(nxterminal_line_length-1); }
+      #endif
+    }
+
+#endif
+
+#if defined(HAVE_SIGNALS) && defined(SIGWINCH) && !defined(NO_ASYNC_INTERRUPTS)
+
+# Signal-Handler für Signal SIGWINCH:
+  local void sigwinch_handler (int sig);
+  local void sigwinch_handler(sig)
+    var int sig; # sig = SIGWINCH
+    { inc_break_sem_5();
+      signal_acknowledge(SIGWINCH,&sigwinch_handler);
+      update_linelength();
+      dec_break_sem_5();
+    }
+
+#define install_sigwinch_handler()  \
+  SIGNAL(SIGWINCH,&sigwinch_handler);
+
+#endif
+
+#if defined(HAVE_SIGNALS) && defined(SIGWINCH) && !defined(NO_ASYNC_INTERRUPTS)
+  # Signal SIGWINCH blockieren, denn eine Veränderung des Wertes von
+  # SYS::*PRIN-LINELENGTH* können wir während der GC nicht brauchen.
+  # Dann Signal SIGWINCH wieder freigeben.
+  #define gc_signalblock_on()  signalblock_on(SIGWINCH)
+  #define gc_signalblock_off()  signalblock_off(SIGWINCH)
+#else
+  #define gc_signalblock_on()
+  #define gc_signalblock_off()
+#endif
