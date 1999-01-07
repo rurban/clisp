cbee64a5cd076e2245038845a51186bbac588a3d
haible <bruno@clisp.org>
1999-01-07 16:29:33 +0000

    Fix compilation problem on glibc2 systems.

--- src/socket.d
+++ src/socket.d
@@ -364,7 +364,9 @@ global SOCKET connect_to_x_server(host,display)
 #include <stdio.h>  # declares sprintf()
 #ifdef HAVE_MEMSET
   #include <string.h>
-  extern_C RETMEMSETTYPE memset (void* ptr, int c, size_t len); # siehe MEMORY(3)
+  #ifndef memset
+    extern_C RETMEMSETTYPE memset (void* ptr, int c, size_t len); # siehe MEMORY(3)
+  #endif
   #define bzero(ptr,len)  memset(ptr,0,len)
   #define bcopy(source,dest,len)  memcpy(dest,source,len)
 #else
