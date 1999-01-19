39c2ecfcf20fed10ce7abce370dfab6c73cd2cd0
Bruno Haible <bruno@clisp.org>
1999-01-19 15:12:35 +0000

    Fix the "strings lisp.run" stupidity :-)

--- src/spvw.d
+++ src/spvw.d
@@ -3226,23 +3226,23 @@ local void usage (int exit_code)
 #endif
 
 static const char * const copyright_notice[] = {
+  "                                                                    \n"
+  "Copyright (c) Bruno Haible, Michael Stoll, Marcus Daniels 1992-1998 \n"
+  "                                                                    \n"
+  "This program is free software; you can redistribute it and/or modify\n"
+  "it under the terms of the GNU General Public License as published by\n"
+  "the Free Software Foundation; either version 2, or (at your option) \n"
+  "any later version.                                                  \n"
+  "                                                                    \n"
+  "This program is distributed in the hope that it will be useful, but \n"
+  "WITHOUT ANY WARRANTY; without even the implied warranty of          \n"
+  "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU   \n"
+  "General Public License for more details.                            \n"
+  "                                                                    \n"
+  "You should have received a copy of the GNU General Public License   \n"
+  "along with this program; if not, write to the Free Software         \n"
+  "Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.\n"
   "                                                                    ",
-  "Copyright (c) Bruno Haible, Michael Stoll, Marcus Daniels 1992-1998 ",
-  "                                                                     ",
-  "This program is free software; you can redistribute it and/or modify",
-  "it under the terms of the GNU General Public License as published by",
-  "the Free Software Foundation; either version 2, or (at your option) ",
-  "any later version.                                                  ",
-  "                                                                      ",
-  "This program is distributed in the hope that it will be useful, but ",
-  "WITHOUT ANY WARRANTY; without even the implied warranty of          ",
-  "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU   ",
-  "General Public License for more details.                            ",
-  "                                                                       ",
-  "You should have received a copy of the GNU General Public License   ",
-  "along with this program; if not, write to the Free Software         ",
-  "Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.",
-  "                                                                        ",
   (const char *) &copyright_notice
 };
 
