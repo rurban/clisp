da56ab66de0e28fdeab4b8f610d1114be1a57bd0
haible <bruno@clisp.org>
1999-04-05 21:28:59 +0000

    Update CLX for Unicode. At least it compiles again now, don't know how it runs.

--- modules/clx/new-clx/link.sh
+++ modules/clx/new-clx/link.sh
@@ -4,10 +4,7 @@ file_list="$file_list"' clx.o'
 mod_list="$mod_list"' clx'
 make clisp-module CC="${CC}" CFLAGS="${CFLAGS}" INCLUDES="$absolute_linkkitdir"
 NEW_FILES="$file_list"
-# Normally, the "-L... -lX11" library ought to be part of NEW_LIBS
-# but we don't need it here because lisp.run itself is normally already linked
-# with this library.
-NEW_LIBS="$file_list -lXpm -lXext"
+NEW_LIBS="$file_list -L/usr/X11R6/lib -lXpm -lXext -lX11"
 NEW_MODULES="$mod_list"
 NEW_FILES="$NEW_FILES clx-preload.lsp clx.lsp clx.fas image.lsp image.fas"
 TO_PRELOAD='clx-preload.lsp'
