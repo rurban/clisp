d4942ab66df4ab6ecdc56ec6b9026394c56ebd95
haible <bruno@clisp.org>
1999-05-15 13:28:18 +0000

    It's time to bump the version number now and then.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Generated automatically from src/VERSION. */
-#define VERSION "1999-01-08"
+#define VERSION "1999-05-15"
 #define VERSION_YYYY 1999
 #define VERSION_YYYY_STRING "1999"
-#define VERSION_MM 1
-#define VERSION_DD 8
+#define VERSION_MM 5
+#define VERSION_DD 15
