e42531edd0b5f3d3e90927227f7755815e9420e1
haible <bruno@clisp.org>
1999-05-31 22:12:34 +0000

    Infrastructure for OS-independent assembly language programming.

---
+++ ffcall/callback/trampoline_r/asmsparc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Translate the assembler syntax of sparc assembler programs
+# Usage: asmsparc < sunos4-asm-file > portable-asm-file
+# The portable-asm-file has to be
+#   1. preprocessed,
+#   2. grep -v '^ *#line' | grep -v '^#'
+#   3. sed -e 's,% ,%,g' -e 's,//.*$,,' -e 's,\$,#,g'
+sed -e '# ----------- Remove gcc self-identification' \
+    -e '/gcc2_compiled/d' \
+    -e '/gnu_compiled_c/d' \
+| \
+sed -e '# ----------- Turn # into $, to avoid trouble in preprocessing' \
+    -e 's,#,\$,g' \
+    -e '# ----------- Declare global symbols as functions (we have no variables)' \
+    -e 's/\.global _\([A-Za-z0-9_]*\)$/.global _\1\' \
+    -e '	DECLARE_FUNCTION(\1)/' \
+    -e '# ----------- Global symbols depends on ASM_UNDERSCORE' \
+    -e 's/_\([A-Za-z0-9_:]*\)/C(\1)/'
