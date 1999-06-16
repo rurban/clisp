1a8ade4deebd507e70c7b8ea33436af3c10178ea
haible <bruno@clisp.org>
1999-06-16 12:15:47 +0000

    Configuration results.

---
+++ sigsegv/configurations/i386-pc-solaris2.6/config.h
@@ -0,0 +1,153 @@
+/* config.h.  Generated automatically by configure.  */
+/* Bruno Haible 19.7.1998 */
+
+
+/* UNIX variants */
+
+/* AC_AIX */
+/* Define if on AIX 3. */
+#ifndef _ALL_SOURCE
+#undef _ALL_SOURCE
+#endif
+
+/* AC_ISC_POSIX */
+/* Define if you need to in order for stat and other things to work. */
+#ifndef _POSIX_SOURCE
+#undef _POSIX_SOURCE
+#endif
+
+/* AC_MINIX */
+/* Define if you need to in order for stat and other things to work. */
+#ifndef _POSIX_SOURCE
+#undef _POSIX_SOURCE
+#endif
+/* Define if the system does not provide POSIX.1 features except
+   with this defined. */
+#ifndef _POSIX_1_SOURCE
+#undef _POSIX_1_SOURCE
+#endif
+/* Define if on MINIX. */
+#undef _MINIX
+
+
+/* header files */
+
+/* CL_OPENFLAGS */
+/* Define if you need <sys/file.h> for using open() flags like O_RDWR. */
+#undef OPEN_NEEDS_SYS_FILE_H
+
+
+/* functions and declarations */
+
+/* CL_MEMSET */
+/* Define if you have the memset() function. */
+#define HAVE_MEMSET 1
+/* Define as the return type of memset(). */
+#define RETMEMSETTYPE char*
+
+/* CL_MALLOC */
+/* Define as the return type of malloc(). */
+#define RETMALLOCTYPE void*
+/* Define as the type of `size' in malloc() declaration. */
+#define MALLOC_SIZE_T unsigned int
+
+/* CL_TYPE_SIGNAL */
+/* Define as the return type of signal handlers (int or void). */
+#define RETSIGTYPE void
+/* Define if the declaration of the signal handler function type needs dots. */
+#undef SIGTYPE_DOTS
+
+/* CL_SIGNALBLOCK */
+/* how to block and unblock signals */
+#define SIGNALBLOCK_SYSV 1
+#define SIGNALBLOCK_POSIX 1
+#undef SIGNALBLOCK_BSD
+
+/* CL_SIGNAL_REINSTALL */
+/* Define if signal handlers need to be reinstalled when they are activated. */
+#define SIGNAL_NEED_REINSTALL 1
+
+/* CL_SIGNAL_UNBLOCK */
+/* Define if SIGNALBLOCK_BSD is defined above and
+   signals need to be unblocked when signal handlers are left. */
+#undef SIGNAL_NEED_UNBLOCK
+
+/* CL_SIGNAL_BLOCK_OTHERS */
+/* Define if SIGNALBLOCK_BSD is defined above and
+   other signals need to be unblocked when signal handlers are left. */
+#undef SIGNAL_NEED_UNBLOCK_OTHERS
+
+/* CL_SIGACTION */
+/* Define if you have the sigaction() function. */
+#define HAVE_SIGACTION 1
+
+/* CL_SIGACTION_REINSTALL */
+/* Define if signal handlers installed via sigaction()
+   need to be reinstalled when they are activated. */
+#undef SIGACTION_NEED_REINSTALL
+
+/* CL_SIGACTION_UNBLOCK */
+/* Define if signals need to be unblocked
+   when signal handlers installed via sigaction() are left. */
+#define SIGACTION_NEED_UNBLOCK 1
+
+/* CL_SIGINTERRUPT */
+/* Define if you have the siginterrupt() function. */
+#define HAVE_SIGINTERRUPT 1
+/* Define if you don't have siginterrupt() or sigaction(), but sigvec(). */
+#undef HAVE_SIGVEC
+
+/* CL_SIGALTSTACK */
+/* Define if you have the sigaltstack() function. */
+#define HAVE_SIGALTSTACK 1
+
+/* CL_GETPAGESIZE */
+/* Define if you have getpagesize(). */
+#define HAVE_GETPAGESIZE 1
+/* Define as the return type of getpagesize(). */
+#define RETGETPAGESIZETYPE int
+
+/* CL_VADVISE */
+/* Define if you have the vadvise() system call. */
+#undef HAVE_VADVISE
+
+/* CL_MACH_VM */
+/* Define if you have the vm_allocate() and task_self() functions. */
+#undef HAVE_MACH_VM
+
+/* CL_MMAP */
+/* Define if you have <sys/mman.h> and the mmap() function. */
+#define HAVE_MMAP 1
+/* Define as the return type of mmap(). */
+#define RETMMAPTYPE caddr_t
+/* Define as the type of `addr' in mmap() declaration. */
+#define MMAP_ADDR_T caddr_t
+/* Define as the type of `len' in mmap() declaration. */
+#define MMAP_SIZE_T size_t
+/* Define if <sys/mman.h> defines MAP_ANON and mmaping with MAP_ANON works. */
+#undef HAVE_MMAP_ANON
+/* Define if <sys/mman.h> defines MAP_ANONYMOUS and mmaping with MAP_ANONYMOUS
+   works. */
+#undef HAVE_MMAP_ANONYMOUS
+/* Define if mmaping of the special device /dev/zero works. */
+#define HAVE_MMAP_DEVZERO 1
+/* Define if mmaping of the special device /dev/zero works,
+   but only on addresses < 2^29. */
+#undef HAVE_MMAP_DEVZERO_SUN4_29
+
+/* CL_MPROTECT */
+/* Define if you have the mprotect() function. */
+#define HAVE_MPROTECT 1
+/* Define if you have a working mprotect() function. */
+#define HAVE_WORKING_MPROTECT 1
+/* Define as const if the declaration of mprotect() needs const. */
+#define MPROTECT_CONST 
+
+/* CL_RLIMIT */
+/* Define if you have the setrlimit() function. */
+#define HAVE_SETRLIMIT 1
+/* Define as the type of `resource' in setrlimit() declaration. */
+#define RLIMIT_RESOURCE_T int
+/* Define as const if the declaration of setrlimit() needs const. */
+#define SETRLIMIT_CONST const
+
---
+++ sigsegv/configurations/i386-pc-solaris2.6/sigsegv.h
@@ -0,0 +1,189 @@
+#ifndef _SIGSEGV_H
+#define _SIGSEGV_H
+
+/*
+ * Copyright 1998-1999 Bruno Haible, <haible@clisp.cons.org>
+ *
+ * This is free software distributed under the GNU General Public Licence
+ * described in the file COPYING. Contact the author if you don't have this
+ * or can't live with it. There is ABSOLUTELY NO WARRANTY, explicit or implied,
+ * on this software.
+ */
+
+/*
+ * Portability section:
+ * HAVE_SIGSEGV_RECOVERY  is defined if the system supports catching SIGSEGV.
+ * HAVE_STACK_OVERFLOW_RECOVERY  is defined if stack overflow can be caught.
+ */
+#if defined(__linux__) && (defined(i386) || defined(__i386)) /* Linux */
+#define HAVE_SIGSEGV_RECOVERY
+#endif
+#if defined(__NetBSD__) || defined(__FreeBSD__) /* NetBSD, FreeBSD */
+#define HAVE_SIGSEGV_RECOVERY
+#endif
+#if defined(__linux__) && defined(sparc) && 0 /* Linux */
+/* This used to work. But in Linux-2.0.33 and 2.2.0 a task's tss.sig_address
+ * is passed to the signal handler only if old SunOS signal frames are in use,
+ * and there is no reliable way to force this. Blech. */
+#define HAVE_SIGSEGV_RECOVERY
+#endif
+#if defined(sun) && defined(unix) && (defined(mc68020) || defined(sparc) || (defined(i386) || defined(__i386))) /* SunOS */
+#define HAVE_SIGSEGV_RECOVERY
+#endif
+#if (defined(sgi) || defined(__sgi)) && (defined(SYSTYPE_SVR4) || defined(__SYSTYPE_SVR4)) /* Irix 5 */
+#define HAVE_SIGSEGV_RECOVERY
+#endif
+#if defined(__osf__) /* OSF/1 */
+#define HAVE_SIGSEGV_RECOVERY
+#endif
+#if defined(_AIX) /* AIX */
+#define HAVE_SIGSEGV_RECOVERY
+#endif
+#if defined(NeXT) /* NeXTstep */
+#define HAVE_SIGSEGV_RECOVERY
+#endif
+#if defined(_WIN32) && !defined(__BORLANDC__) /* Win32, but not Borland C */
+#define HAVE_SIGSEGV_RECOVERY
+#define HAVE_STACK_OVERFLOW_RECOVERY
+#endif
+
+/* #undef HAVE_SIGSEGV_RECOVERY here if test1 crashes */
+
+#if defined(__linux__) || (defined(sun) && defined(unix) && (defined(mc68020) || defined(sparc) || (defined(i386) || defined(__i386)))) || ((defined(sgi) || defined(__sgi)) && (defined(SYSTYPE_SVR4) || defined(__SYSTYPE_SVR4))) || defined(__osf__) /* Linux, SunOS, Irix, OSF/1 */
+#define HAVE_STACK_OVERFLOW_RECOVERY
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* -------------------------------------------------------------------------- */
+
+/*
+ * The type of a global SIGSEGV handler.
+ * The fault address is passed as argument.
+ * The access type (read access or write access) is not passed; your handler
+ * has to know itself how to distinguish these two cases.
+ * The second argument is 0, meaning it could also be a stack overflow, or 1,
+ * meaning the handler should seriously try to fix the fault.
+ * The return value should be nonzero if the handler has done its job
+ * and no other handler should be called, or 0 if the handler declines
+ * responsibility for the given address.
+ */
+typedef int (*sigsegv_handler_t) (void* fault_address, int serious);
+
+/*
+ * Installs a global SIGSEGV handler.
+ * This should be called once only, and it ignores any previously installed
+ * SIGSEGV handler.
+ * Returns 0 on success, or -1 if the system doesn't support catching SIGSEGV.
+ */
+extern int sigsegv_install_handler (sigsegv_handler_t handler);
+
+/*
+ * Deinstalls the global SIGSEGV handler.
+ * This goes back to the state where no SIGSEGV handler is installed.
+ */
+extern void sigsegv_deinstall_handler (void);
+
+/*
+ * Prepares leaving a SIGSEGV handler (through longjmp or similar means).
+ * On Unix, this unblocks some signals.
+ */
+extern void sigsegv_leave_handler (void);
+
+/*
+ * The type of a context passed to a stack overflow handler.
+ */
+#if defined(__linux__) && (defined(i386) || defined(__i386) || defined(m68k) || defined(__m68k) || defined(mips) || defined(__mips) || defined(sparc) || defined(__sparc) || defined(alpha) || defined(__alpha) || defined(arm) || defined(__arm))
+typedef struct sigcontext * stackoverflow_context_t;
+#else
+typedef void * stackoverflow_context_t;
+#endif
+
+/*
+ * The type of a stack overflow handler.
+ * Such a handler should perform a longjmp call in order to reduce the amount
+ * of stack needed. It must not return.
+ * The emergency argument is 0 when the stack could be repared, or 1 if the
+ * application should better save its state and exit now.
+ */
+typedef void (*stackoverflow_handler_t) (int emergency, stackoverflow_context_t scp);
+
+/*
+ * Installs a stack overflow handler.
+ * The extra_stack argument is a pointer to a pre-allocated area used as a
+ * stack for executing the handler. It is typically allocated by use of
+ * `alloca' during `main'. Its size should be sufficiently large (typically
+ * 16 KB).
+ * Returns 0 on success, or -1 if the system doesn't support catching stack
+ * overflow.
+ */
+extern int stackoverflow_install_handler (stackoverflow_handler_t handler,
+                                          void* extra_stack, unsigned long extra_stack_size);
+
+/*
+ * Deinstalls the stack overflow handler.
+ */
+extern void stackoverflow_deinstall_handler (void);
+
+/* -------------------------------------------------------------------------- */
+
+/*
+ * The following structure and functions permit to define different SIGSEGV
+ * policies on different address ranges.
+ */
+
+/*
+ * The type of a local SIGSEGV handler.
+ * The fault address is passed as argument.
+ * The second argument is fixed arbitrary user data.
+ * The return value should be nonzero if the handler has done its job
+ * and no other handler should be called, or 0 if the handler declines
+ * responsibility for the given address.
+ */
+typedef int (*sigsegv_area_handler_t) (void* fault_address, void* user_arg);
+
+/*
+ * This structure represents a table of memory areas (address range intervals),
+ * with an local SIGSEGV handler for each.
+ */
+typedef
+struct sigsegv_dispatcher {
+  void* tree;
+}
+sigsegv_dispatcher;
+
+/*
+ * Initializes a sigsegv_dispatcher structure.
+ */
+extern void sigsegv_init (sigsegv_dispatcher* dispatcher);
+
+/*
+ * Adds a local SIGSEGV handler to a sigsegv_dispatcher structure.
+ * It will cover the interval [address..address+len-1].
+ * Returns a "ticket" that can be used to remove the handler later.
+ */
+extern void* sigsegv_register (sigsegv_dispatcher* dispatcher,
+                               void* address, unsigned long len,
+                               sigsegv_area_handler_t handler, void* handler_arg);
+
+/*
+ * Removes a local SIGSEGV handler.
+ */
+extern void sigsegv_unregister (sigsegv_dispatcher* dispatcher, void* ticket);
+
+/*
+ * Call the local SIGSEGV handler responsible for the given fault address.
+ * Return the handler's return value. 0 means that no handler has been found,
+ * or that a handler was found but declined responsibility.
+ */
+extern int sigsegv_dispatch (sigsegv_dispatcher* dispatcher, void* fault_address);
+
+/* -------------------------------------------------------------------------- */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SIGSEGV_H */
