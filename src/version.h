4f7857fb8c7ff5c22da7fdd8c3dc60d0e6ea1c3d
haible <bruno@clisp.org>
2000-03-02 13:25:58 +0000

    Regenerated.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Generated automatically from src/VERSION. */
-#define VERSION "2000-01-25"
+#define VERSION "2000-03-06"
 #define VERSION_YYYY 2000
 #define VERSION_YYYY_STRING "2000"
-#define VERSION_MM 1
-#define VERSION_DD 25
+#define VERSION_MM 3
+#define VERSION_DD 6
