e0f55869580cc9887571e64b192e35cee462a7b3
haible <unknown>
2000-03-07 20:56:23 +0000

    Regenerated with gcc-2.95.2.

--- ffcall/trampoline/proto-sparc.s
+++ ffcall/trampoline/proto-sparc.s
@@ -6,26 +6,25 @@ ___gnu_compiled_c:
 	.proc	04
 _tramp:
 	!#PROLOGUE# 0
-	save %sp,-104,%sp
+	save	%sp, -104, %sp
 	!#PROLOGUE# 1
-	sethi %hi(305419896),%o0
-	sethi %hi(1934968593),%o1
-	or %o1,%lo(1934968593),%o1
-	st %o1,[%o0+%lo(305419896)]
-	sethi %hi(-1161904448),%o0
-	call %o0+%lo(-1161904448),0
-	nop
-	mov %o0,%i0
+	sethi	%hi(305419264), %o2
+	sethi	%hi(1934967808), %o1
+	or	%o2, 632, %o2
+	or	%o1, 785, %o1
+	sethi	%hi(-1161905152), %o0
+	or	%o0, 704, %o0
+	call	%o0, 0
+	st	%o1, [%o2]
 	ret
-	restore
+	restore %g0, %o0, %o0
 	.align 4
 	.global _jump
 	.proc	04
 _jump:
 	!#PROLOGUE# 0
 	!#PROLOGUE# 1
-	sethi %hi(-1161904448),%g2
-	jmp %g2+%lo(-1161904448)
-	nop
-	retl
-	nop
+	sethi	%hi(-1161905152), %g2
+	or	%g2, 704, %g2
+	jmp	%g2
+	 nop
