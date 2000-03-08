f167a5c130e11f0b3c3467257af206672399f69e
Bruno Haible <bruno@clisp.org>
2000-03-08 19:29:03 +0000

    Add files from the DOS port.

--- os2/copyx.cmd
+++ os2/copyx.cmd
@@ -1,11 +1,11 @@
 REM copies some OS/2 specific files into SRC
 copy os2\forall.cmd src
-copy dos\*.in src
+copy os2\*.in src
 copy os2\makefile src
 copy src\readline\makefile.os2 src\readline\makefile
 copy src\readline\examples\makefile.dos src\readline\examples\makefile
 copy src\newreadline\makefile.os2 src\newreadline\makefile
-copy dos\rlconfig.h src\newreadline\config.h
+copy os2\rlconfig.h src\newreadline\config.h
 copy src\gettext\intl\makefile.dos src\gettext\intl\makefile
-copy dos\gtobjects src\gettext\intl\objects
-copy dos\gtconfig.h src\gettext\config.h
+copy os2\gtobjects src\gettext\intl\objects
+copy os2\gtconfig.h src\gettext\config.h
