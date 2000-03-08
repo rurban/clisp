e97e9539b35b643877821721ec4324c7f5a16442
Sam Steingold <sds@gnu.org>
2000-03-08 20:58:35 +0000

    banner copyright year update

--- src/spvw.d
+++ src/spvw.d
@@ -1733,7 +1733,7 @@ local void print_banner ()
    "Copyright (c) Bruno Haible, Michael Stoll 1992, 1993" NLstring,
    "Copyright (c) Bruno Haible, Marcus Daniels 1994-1997" NLstring,
    "Copyright (c) Bruno Haible, Pierpaolo Bernardi, Sam Steingold 1998" NLstring,
-   "Copyright (c) Bruno Haible, Sam Steingold 1999" NLstring,
+   "Copyright (c) Bruno Haible, Sam Steingold 1999-2000" NLstring,
   };
   #ifdef AMIGA
   var const char * banner2 =
