0777f491814c773b4367fb319f6e3d891bf73bb2
haible <bruno@clisp.org>
2000-03-09 20:35:48 +0000

    Bump version number.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Generated automatically from src/VERSION. */
-#define VERSION "2000-03-06"
+#define VERSION "2000-03-09"
 #define VERSION_YYYY 2000
 #define VERSION_YYYY_STRING "2000"
 #define VERSION_MM 3
-#define VERSION_DD 6
+#define VERSION_DD 9
