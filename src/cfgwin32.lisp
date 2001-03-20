c7ee8b90e13c9d3cc2dd6f5feab96ba97a20a991
Bruno Haible <bruno@clisp.org>
2001-03-20 19:31:29 +0000

    Use a trailing slash in the CLHS URL; www.lisp.org needs it.

--- src/cfgwin32.lisp
+++ src/cfgwin32.lisp
@@ -92,4 +92,4 @@ in ~/.clisprc.  On win32 you can also use the Registry."
         (check-type s (or null string))
         s)
       *clhs-root-default*))
-(setq *clhs-root-default* "http://www.lisp.org/HyperSpec")
+(setq *clhs-root-default* "http://www.lisp.org/HyperSpec/")
