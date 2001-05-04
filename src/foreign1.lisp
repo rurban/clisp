71961a8844d5629050713dad3c5c67af3d238c81
Sam Steingold <sds@gnu.org>
2001-05-04 19:31:57 +0000

    (argvector-to-signature): signatures are structs!

--- src/foreign1.lisp
+++ src/foreign1.lisp
@@ -1024,7 +1024,7 @@ void module__~A__init_function_2(module)
 ;; ===========================================================================
 
 (defun argvector-to-signature (argvector)
-  (list (count-inarguments argvector) 0 nil nil nil nil))
+  (make-signature :req-num (count-inarguments argvector)))
 
 (defun count-inarguments (arg-vector)
   (do* ((l (length arg-vector))
