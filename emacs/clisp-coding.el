5b9492dd0aad9786e1a663c88d74ff3ae9aff1d5
Sam Steingold <sds@gnu.org>
2001-06-12 14:40:26 +0000

    add (modify-coding-system-alist 'process "" 'unix) for win32

--- emacs/clisp-coding.el
+++ emacs/clisp-coding.el
@@ -2,12 +2,8 @@
 ;;; Load this file from ~/.emacs or ~/.emacs.el
 ;;; Tested with Emacs 20 with Mule-UCS, Emacs 21
 
-(defun clisp-find-file-coding-system (arg-list)
-  (and (eq (car arg-list) 'insert-file-contents)
-       'utf-8))
+;; All the *.d and *.lisp sources are in UTF-8 encoding.
+(modify-coding-system-alist 'file "\\.\\(d\\|lisp\\)\\'" 'utf-8)
 
-; All the *.d sources are in UTF-8 encoding.
-(modify-coding-system-alist 'file "\\.d\\'" 'clisp-find-file-coding-system)
-
-; All the *.lisp sources are in UTF-8 encoding.
-(modify-coding-system-alist 'file "\\.lisp\\'" 'clisp-find-file-coding-system)
+;; For CLISP in `inferior-lisp-mode' under win32
+(modify-coding-system-alist 'process "lisp" 'unix)
