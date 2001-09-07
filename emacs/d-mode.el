75773641d2735eb384294e1bd9014f3d823a807c
sds <sds@podval.org>
2001-09-07 19:47:20 +0000

    (d-font-lock-extra-types, d-extra-keywords): new variables (d-mode-modify-font-lock): renamed from `d-mode-modify-lock-1'  also replace `c-font-lock-extra-types' with `d-font-lock-extra-types' (c-C-specifier-kwds): added `d-extra-keywords'

--- emacs/d-mode.el
+++ emacs/d-mode.el
@@ -5,22 +5,35 @@
 
 (require 'c-mode)
 
-(defun d-mode-modify-lock-1 (form)
-  "`d-mode' should highlight #foo not only at the beginning-of-line"
-  (if (and (consp form) (stringp (car form))
-           (= ?^ (aref (car form) 0))
-           (= ?# (aref (car form) 1)))
-      (cons (concat "^[ \t]*" (substring (car form) 1)) (cdr form))
-      form))
+(defvar d-font-lock-extra-types
+  '(nconc (list "object" "chart" "signean" "u?int[LB0-9]*")
+    c-font-lock-extra-types)
+  "Extra types to be fontified as such.")
+
+(defun d-mode-modify-font-lock (form)
+  "Modify the font locking spec appropriately."
+  (subst d-font-lock-extra-types 'c-font-lock-extra-types
+         ;; `d-mode' should highlight #foo not only at the beginning-of-line
+         (if (and (consp form) (stringp (car form))
+                  (= ?^ (aref (car form) 0))
+                  (= ?# (aref (car form) 1)))
+             (cons (concat "^[ \t]*" (substring (car form) 1)) (cdr form))
+             form)))
+
+(defvar d-extra-keywords
+  (eval-when-compile
+   (regexp-opt '("var" "local" "global" "dotimes[a-zA-Z]*"))))
 
 (defvar d-font-lock-keywords-1
-  (mapcar #'d-mode-modify-lock-1 c-font-lock-keywords-1))
+  (mapcar #'d-mode-modify-font-lock c-font-lock-keywords-1))
 
 (defvar d-font-lock-keywords-2
-  (mapcar #'d-mode-modify-lock-1 c-font-lock-keywords-2))
+  (cons (concat "\\<\\(" d-extra-keywords "\\)\\>")
+        (mapcar #'d-mode-modify-font-lock c-font-lock-keywords-2)))
 
 (defvar d-font-lock-keywords-3
-  (mapcar #'d-mode-modify-lock-1 c-font-lock-keywords-3))
+  (cons (concat "\\<\\(" d-extra-keywords "\\)\\>")
+        (mapcar #'d-mode-modify-font-lock c-font-lock-keywords-3)))
 
 (defvar d-font-lock-keywords d-font-lock-keywords-1)
 
@@ -43,13 +56,13 @@ Turning on D mode calls the value of the variable `d-mode-hook',
 if that value is non-nil.
 If you are using Emacs 20.2 or earlier (including XEmacs) and want to
 use fontifications, you have to (require 'font-lock) first.  Sorry.
-You might want to do also
-  (add-to-list 'c-font-lock-extra-types \"object\")
 Beware - this will modify the original C-mode too!"
-  (set (make-local-variable 'font-lock-defaults)
-       d-mode-font-lock-defaults))
+  (when (<= 21 emacs-major-version)
+    (set (make-local-variable 'font-lock-defaults)
+         d-mode-font-lock-defaults)))
 
 (when window-system
+  ;; enable font locking
   (if sds-xemacs
       (put 'd-mode 'font-lock-defaults d-mode-font-lock-defaults)
       (when (and (> 21 emacs-major-version)
@@ -57,6 +70,8 @@ Beware - this will modify the original C-mode too!"
         (setq font-lock-defaults-alist
               (cons (cons 'd-mode d-mode-font-lock-defaults)
                     font-lock-defaults-alist)))))
+
+(setq c-C-specifier-kwds (concat c-C-specifier-kwds "\\|" d-extra-keywords))
 
 ;; enable CLISP "# foo" comments
 (modify-syntax-entry ?# ". 1b" d-mode-syntax-table)
