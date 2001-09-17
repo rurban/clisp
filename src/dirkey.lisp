96d3b9708f9fad9b3aa0b91ad4e9904c4239bd7d
sds <sds@podval.org>
2001-09-17 15:59:12 +0000

    added (C)

--- src/dirkey.lisp
+++ src/dirkey.lisp
@@ -1,4 +1,4 @@
-;;; Copyright (C) 2000 by Sam Steingold
+;;; Copyright (C) 2000-2001 by Sam Steingold
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 
