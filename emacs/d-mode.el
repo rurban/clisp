48ab941cb6bbb024e1c4ed002c30c0c356225338
Sam Steingold <sds@gnu.org>
2002-01-28 17:09:37 +0000

    (d-font-lock-extra-types): support more int-derivatives

--- emacs/d-mode.el
+++ emacs/d-mode.el
@@ -70,7 +70,7 @@ The point should be on the prototype and the definition should follow."
 
 (defvar d-font-lock-extra-types
   '(nconc (list "bool" "object" "chart" "[otac]int" "signean" "scint"
-           "[su]?int[BCL0-9]*" "Values" "fsubr_function" "lisp_function")
+           "[su]?int[BCLPW0-9]*" "Values" "fsubr_function" "lisp_function")
     c-font-lock-extra-types)
   "Extra types to be fontified as such.")
 
