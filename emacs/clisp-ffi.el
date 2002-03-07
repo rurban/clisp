5c3bf54365e24bc3757c8de7e60e312453962c49
Sam Steingold <sds@gnu.org>
2002-03-07 16:50:16 +0000

    syntax highlighting for CLISP FFI forms

---
+++ emacs/clisp-ffi.el
@@ -0,0 +1,13 @@
+;;; syntax highlighting for CLISP FFI forms            -*- Emacs-Lisp -*-
+;;; Load this file from ~/.emacs or ~/.emacs.el
+
+(defconst clisp-ffi-font-lock-keywords
+  '("(\\(def-c-\\(\\(call\\(\\s_\\|\\sw\\)*\\)\\|\\(var\\)\\|\\(enum\\|struct\\|type\\)\\)\\)\\s *\\(\\(\\s_\\|\\sw\\)*\\)"
+    (1 font-lock-keyword-face)
+    (7 (cond ((match-beginning 3) font-lock-function-name-face)
+             ((match-beginning 5) font-lock-variable-name-face)
+             (t font-lock-type-face)) nil t))
+  "Additional lisp-mode keywords for CLISP FFI forms.")
+
+(setq lisp-font-lock-keywords
+      (cons clisp-ffi-font-lock-keywords lisp-font-lock-keywords-2))
