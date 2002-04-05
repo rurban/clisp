0269d9ebcd141cba9f569c78380d08989be74eb7
sds <unknown>
2002-04-05 20:51:19 +0000

    include <stdlib.h> before "regex.h" From Joerg-Cyril Hoehle <Joerg-Cyril.Hoehle@t-systems.com>

--- modules/regexp/regexi.c
+++ modules/regexp/regexi.c
@@ -2,15 +2,10 @@
 /* Bruno Haible 14.4.1995 */
 
 #include <sys/types.h> /* regex.h needs this */
+#include <stdlib.h> /* declare malloc(), free() */
 #include "regex.h"
 
-#include <stdlib.h> /* declare malloc(), free() */
-
-int mregcomp (ppreg,pattern,cflags)
-  regex_t* * ppreg;
-  const char * pattern;
-  int cflags;
-{
+int mregcomp (regex_t* * ppreg, const char * pattern, int cflags) {
   regex_t* preg;
 
   *ppreg = preg = (regex_t*) malloc(sizeof(regex_t));
@@ -20,10 +15,7 @@ int mregcomp (ppreg,pattern,cflags)
   return regcomp(preg,pattern,cflags);
 }
 
-const char * mregerror (errcode,preg)
-  int errcode;
-  const regex_t * preg;
-{
+const char * mregerror (int errcode, const regex_t * preg) {
   size_t errbuf_size = 80; /* This will be enough. */
   char* errbuf = (char*) malloc(errbuf_size);
   if (!errbuf)
@@ -32,9 +24,7 @@ const char * mregerror (errcode,preg)
   return errbuf;
 }
 
-void mregfree (preg)
-  regex_t* preg;
-{
+void mregfree (regex_t* preg) {
   regfree(preg);
   free(preg);
 }
