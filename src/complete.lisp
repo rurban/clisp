c1d0c0a147b58c62531ba569216e2c6c18ad3c93
Sam Steingold <sds@gnu.org>
2002-06-11 21:24:46 +0000

    (completion): restored adding ")" when completing a function without arguments, broken since FUNCTION-SIGNATURE was modified to return the function name as the first value

--- src/complete.lisp
+++ src/complete.lisp
@@ -3,111 +3,83 @@
 (in-package "SYSTEM")
 
 ;;-----------------------------------------------------------------------------
-
-; Vervollständigungs-Routine in Verbindung mit der GNU Readline-Library:
-; Input: string die Eingabezeile, (subseq string start end) das zu vervoll-
-; ständigende Textstück.
-; Output: eine Liste von Simple-Strings. Leer, falls keine sinnvolle Vervoll-
-; ständigung. Sonst CDR = Liste aller sinnvollen Vervollständigungen, CAR =
-; sofortige Ersetzung.
+;; Completing routine for the GNU Readline library.
+;; Input: string (the input line), and the boundaries of the text to be
+;; completed:  (subseq string start end)
+;; Output: a list of simple-strings. empty, when no meaningful completions.
+;; otherwise, CDR = list of all meaningful completions,
+;; CAR = the immediate replacement
 #+(or UNIX OS/2)
 (defun completion (string start end)
-  ; quotiert vervollständigen?
-  (let ((start1 start) (quoted nil))
+  (let* ((start1 start) (quotedp nil) ; quoted completion?
+         ;; vars for collecting the symbols
+         known-part known-len (return-list '())
+         (functionalp1 (and (>= start1 1)
+                            (equal (subseq string (- start1 1) start1) "(")))
+         (functionalp2 (and (>= start1 2)
+                            (equal (subseq string (- start1 2) start1) "#'")))
+         ;; completion of a function
+         (functionalp (or functionalp1 functionalp2))
+         (gatherer
+          (if functionalp
+              (lambda (sym)
+                (when (fboundp sym)
+                  (let ((name (symbol-name sym)))
+                    (when (and (>= (length name) known-len)
+                               (string-equal name known-part :end1 known-len))
+                      (push name return-list)))))
+              (lambda (sym)
+                (let ((name (symbol-name sym)))
+                  (when (and (>= (length name) known-len)
+                             (string-equal name known-part :end1 known-len))
+                    (push name return-list))))))
+         (package *package*)
+         (mapfun #'sys::map-symbols)
+         (prefix nil))
     (when (and (>= start 1) (member (char string (- start 1)) '(#\" #\|)))
-      (decf start1) (setq quoted t)
-    )
-    (let (; Hilfsvariablen beim Sammeln der Symbole:
-          knownpart ; Anfangsstück
-          knownlen  ; dessen Länge
-          (L '())   ; sammelnde Liste
-         )
-      (let* ((functionalp1
-               (and (>= start1 1)
-                    (equal (subseq string (- start1 1) start1) "(")
-             ) )
-             (functionalp2
-               (and (>= start1 2)
-                    (equal (subseq string (- start1 2) start1) "#'")
-             ) )
-             (functionalp ; Vervollständigung in funktionaler Position?
-               (or functionalp1 functionalp2)
-             )
-             (gatherer
-               (if functionalp
-                 #'(lambda (sym)
-                     (when (fboundp sym)
-                       (let ((name (symbol-name sym)))
-                         (when (and (>= (length name) knownlen) (string-equal name knownpart :end1 knownlen))
-                           (push name L)
-                   ) ) ) )
-                 #'(lambda (sym)
-                     (let ((name (symbol-name sym)))
-                       (when (and (>= (length name) knownlen) (string-equal name knownpart :end1 knownlen))
-                         (push name L)
-                   ) ) )
-             ) )
-             (package *package*)
-             (mapfun #'sys::map-symbols)
-             (prefix nil))
-        ; Evtl. Packagenamen abspalten:
-        (unless quoted
-          (let ((colon (position #\: string :start start :end end)))
-            (when colon
-              (unless (setq package (find-package (string-upcase (subseq string start colon))))
-                (return-from completion nil)
-              )
+      (decf start1) (setq quotedp t))
+    ;; get the package name:
+    (unless quotedp
+      (let ((colon (position #\: string :start start :end end)))
+        (when colon
+          (unless (setq package (find-package (string-upcase
+                                               (subseq string start colon))))
+            (return-from completion nil))
+          (incf colon)
+          (if (and (< colon end) (eql (char string colon) #\:))
               (incf colon)
-              (if (and (< colon end) (eql (char string colon) #\:))
-                (incf colon)
-                (setq mapfun #'sys::map-external-symbols)
-              )
-              (setq prefix (subseq string start colon))
-              (setq start colon)
-        ) ) )
-        (setq knownpart (subseq string start end))
-        (setq knownlen (length knownpart))
-        (funcall mapfun gatherer package)
-        (when (null L) (return-from completion nil))
-        ; Bei einer Funktion ohne Argumente ergänze die schließende Klammer:
-        (when (and functionalp1
-                   (null (cdr L))
-                   (let ((sym (find-symbol (car L) package)))
-                     (and #| sym |#
-                          (fboundp sym)
-                          (functionp (symbol-function sym))
-                          (multiple-value-bind (req-anz opt-anz rest-p key-p)
-                              (function-signature (symbol-function sym))
-                            (and (eql req-anz 0) (eql opt-anz 0) (not rest-p) (not key-p))
-              )    ) )    )
-          (setf (car L) (string-concat (car L) ")"))
-        )
-        ; Kleinbuchstaben:
-        (unless quoted
-          (setq L (mapcar #'string-downcase L))
-        )
-        ; sortieren:
-        (setq L (sort L #'string<))
-        ; größtes gemeinsames Anfangsstück suchen:
-        (let ((imax ; (reduce #'min (mapcar #'length L))
-                (let ((i (length (first L))))
-                  (dolist (s (rest L)) (setq i (min i (length s))))
-                  i
-             )) )
-          (do ((i 0 (1+ i)))
-              ((or (eql i imax)
-                   (let ((c (char (first L) i)))
-                     (dolist (s (rest L) nil) (unless (eql (char s i) c) (return t)))
-               )   )
-               (push (subseq (first L) 0 i) L)
-        ) )   )
-        ; Präfix wieder ankleben:
-        (when prefix
-          (mapl #'(lambda (l)
-                    (setf (car l) (string-concat prefix (car l)))
-                  )
-                L
-        ) )
-        L
-) ) ) )
-
+              (setq mapfun #'sys::map-external-symbols))
+          (setq prefix (subseq string start colon))
+          (setq start colon))))
+    (setq known-part (subseq string start end))
+    (setq known-len (length known-part))
+    (funcall mapfun gatherer package)
+    (when (null return-list) (return-from completion nil))
+    ;; for a function without arguments, return the closing paren
+    (when (and functionalp1
+               (null (cdr return-list))
+               (let ((sym (find-symbol (car return-list) package)))
+                 (and sym (fboundp sym)
+                      (functionp (symbol-function sym))
+                      (multiple-value-bind (name req-anz opt-anz rest-p key-p)
+                          (function-signature (symbol-function sym))
+                        (declare (ignore name))
+                        (and (eql req-anz 0) (eql opt-anz 0)
+                             (not rest-p) (not key-p))))))
+      (setf (car return-list) (string-concat (car return-list) ")")))
+    (unless quotedp             ; downcase
+      (setq return-list (mapcar #'string-downcase return-list)))
+    (setq return-list (sort return-list #'string<))
+    ;; look for the largest common initial piece
+    (let ((imax (reduce #'min return-list :key #'length)))
+      (do ((i 0 (1+ i)))
+          ((or (eql i imax)
+               (let ((c (char (first return-list) i)))
+                 (dolist (s (rest return-list) nil)
+                   (unless (eql (char s i) c) (return t)))))
+           (push (subseq (first return-list) 0 i) return-list))))
+    ;; reattach prefix
+    (when prefix
+      (mapl #'(lambda (l) (setf (car l) (string-concat prefix (car l))))
+            return-list))
+    return-list))
