0214b74890f951d1ced25426bc07f24c39feca8d
hin <unknown>
2003-01-09 20:16:46 +0000

    Initial release of a module to talk to FastCGI Web server interface.

---
+++ modules/fastcgi/link.sh
@@ -0,0 +1,7 @@
+# link.sh for FastCGI library
+# $Id$
+make clisp-module CC="${CC}" CFLAGS="${CFLAGS}" INCLUDES="$absolute_linkkitdir"
+NEW_FILES="fastcgi.o fastcgi_wrappers.o"
+NEW_LIBS="fastcgi.o fastcgi_wrappers.o -lfcgi"
+NEW_MODULES="fastcgi"
+TO_LOAD="fastcgi"
