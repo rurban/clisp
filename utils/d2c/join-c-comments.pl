84c2b13ddd6eb485af0f18d5c134cc81b39963b3
Peter Seibel <peter@javamonkey.com>
2003-03-01 23:42:47 +0000

    Updating d2c directory with some improvements to code formatting. Also backing out changes that converted from utf8 to ascii.

--- utils/d2c/join-c-comments.pl
+++ utils/d2c/join-c-comments.pl
@@ -8,12 +8,25 @@
 use strict;
 
 my @line_buffer = ();
-my $header_comment = undef;
+my $indent = undef;
+my $header_comment = 1;
+my $add_to_header_comment = undef;
 
 while (<>) {
   m!^#line 1 ".*?\.d"$! and next;
-  if (m!^\s*/\*.*\*/\s*$!) {
+  if (m!^(\s*)/\*.*\*/\s*$!) {
+    my $i = length($1);
+    if (not defined $indent) {
+      $indent = $i;
+    }
+
+    if ($i != $indent) {
+      dump_line_buffer();
+      $indent = $i;
+    }
+
     push @line_buffer, $_;
+
   } else {
     dump_line_buffer();
     print;
@@ -21,26 +34,50 @@ while (<>) {
 }
 dump_line_buffer();
 
+
 sub dump_line_buffer {
   if (@line_buffer) {
     if ($header_comment) {
-      push @line_buffer, $header_comment;
-      undef $header_comment;
-    }
-    if ($#line_buffer == 0) {
+      $add_to_header_comment and push @line_buffer, $add_to_header_comment;
+      $header_comment = 0;
+      tidy_comment(@line_buffer);
+    } elsif ($#line_buffer == 0) {
       print $line_buffer[0];
     } else {
-      print "/*";
-      for (my $i = 0; $i <= $#line_buffer; $i++) {
-        my $line = $line_buffer[$i];
-        my ($guts) = $line =~ m!/\*(.*?)\s*\*/!;
-        print "$guts";
-        if ($i < $#line_buffer) { print "\n"; }
-      }
-      print " */\n";
+      compact_comment(@line_buffer);
     }
     @line_buffer = ();
+    $indent = undef;
   }
+}
+
+
+sub compact_comment {
+  my (@line_buffer) = @_;
+  print " " x $indent;
+  print "/*";
+  for (my $i = 0; $i <= $#line_buffer; $i++) {
+    my $line = $line_buffer[$i];
+    my ($guts) = $line =~ m!/\*(.*?)\s*\*/!;
+    unless ($i == 0) { print " " x $indent; }
+    print "$guts";
+    if ($i < $#line_buffer) { print "\n"; }
+  }
+  print " */\n";
+}
+
+sub tidy_comment {
+  my (@line_buffer) = @_;
+  print " " x $indent;
+  print "/*\n";
+  for (my $i = 0; $i <= $#line_buffer; $i++) {
+    my $line = $line_buffer[$i];
+    my ($guts) = $line =~ m!/\*(.*?)\s*\*/!;
+    print " " x $indent;
+    print " * $guts\n";
+  }
+  print " " x $indent;
+  print " */\n";
 }
 
 __END__
