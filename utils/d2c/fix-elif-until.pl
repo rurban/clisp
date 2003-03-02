3ff5fbe4bb9cfc45b34da908f7c420fe46e82df3
peterseibel <unknown>
2003-03-02 05:23:10 +0000

    Fixing up fix-elif-until.pl to not turn CPP elif's into else if's.

--- utils/d2c/fix-elif-until.pl
+++ utils/d2c/fix-elif-until.pl
@@ -8,6 +8,7 @@ my $text = <>;
 $text =~ s/^\s*\#\s*define\s+(until|elif|loop)\b.*$//gm;
 $text =~ s/(\s+)elif\b/$1else if/g;
 $text =~ s/\bloop\b/while (1)/g;
+$text =~ s/^(\#\s*)else if/$1elif/gm;
 
 OUTER:
 while ($text =~ m/until\s*/g) {
@@ -33,42 +34,3 @@ print $text;
 
 __END__
 
-while (<>) {
-  #/^\s*\#\s*define\s+(local|global|var|until|elif)\b/ and next;
-  /^\s*\#\s*define\s+(until|elif)\b/ and next;
-  s/(\s+)elif\b/$1else if/g;
-  #s/\blocal\b/static/g;
-  #s/\bglobal\b//g;
-  #s/\bvar\b//g;
-  if (/until\s*\(/) {
-    fix_until_line($_);
-  } else {
-    print;
-  }
-}
-
-sub fix_untils {
-  my ($line) = @_;
- OUTER:
-  while ($line =~ m/until\s*/g) {
-    my $pos = pos $line;
-    my $end = length $line;
-    my $parens = 0;
-    for (my $i = $pos; $i < $end; $i++) {
-      my $c = substr($line, $i, 1);
-      if ($c eq '(') { $parens++; }
-      if ($c eq ')') { $parens--; }
-      if ($parens == 0) {
-        substr($line, $i, 0, ")");
-        substr($line, $pos, 0, "(!");
-        $line =~ s/until(\s*)/while$1/;
-        pos $line = $i + 3;
-        next OUTER;
-      }
-    }
-  }
-  print $line;
-}
-
-
-__END__
