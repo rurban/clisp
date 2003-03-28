27d1786497f54bbe387c64cfc455b05599924c1a
Sam Steingold <sds@gnu.org>
2003-03-28 17:34:45 +0000

    support def-c-struct options

--- emacs/clisp-ffi.el
+++ emacs/clisp-ffi.el
@@ -3,7 +3,7 @@
 
 (font-lock-add-keywords
  'lisp-mode
- '(("(\\(def-\\(\\(call\\(\\s_\\|\\sw\\)*\\)\\|\\(c-var\\)\\|\\(c-enum\\|c-struct\\|c-type\\)\\)\\)\\s *\\(\\(\\s_\\|\\sw\\)*\\)"
+ '(("(\\(def-\\(\\(call\\(\\s_\\|\\sw\\)*\\)\\|\\(c-var\\)\\|\\(c-enum\\|c-struct\\|c-type\\)\\)\\)\\>[ \t'\(]*\\(\\(\\s_\\|\\sw\\)*\\)"
     (1 font-lock-keyword-face)
     (7 (cond ((match-beginning 3) font-lock-function-name-face)
              ((match-beginning 5) font-lock-variable-name-face)
