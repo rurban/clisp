7a4f3ae8de53886cc2492bff826dd7ce927497d0
Sam Steingold <sds@gnu.org>
2003-07-25 20:27:51 +0000

    (NEW_LIBS): added wldap32.lib

--- modules/dirkey/link.sh
+++ modules/dirkey/link.sh
@@ -1,7 +1,7 @@
 files='dirkey.o'
 make clisp-module CC="${CC}" CFLAGS="${CFLAGS}" INCLUDES="$absolute_linkkitdir"
 NEW_FILES="${files}"
-NEW_LIBS="${files}"
+NEW_LIBS="${files} wldap32.lib"
 NEW_MODULES='dirkey'
 TO_LOAD='dirkey1'
 TO_PRELOAD="preload.lisp"
