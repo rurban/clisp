b6a54974792c782eb9703dcd8c3bc5087bcef402
Sam Steingold <sds@gnu.org>
2003-08-06 16:04:53 +0000

    Unified modules postgresql632 and postgresql642 into postgresql

--- modules/postgresql/sql.lisp
+++ modules/postgresql/sql.lisp
@@ -1,16 +1,21 @@
-;;; PostgreSQL test
+;;; PostgreSQL demo
 ;;; Based on the examples distributed with PostgreSQL (man libpq)
 ;;;
-;;; Copyright (C) 1999 by Sam Steingold
+;;; Copyright (C) 1999-2003 by Sam Steingold
 ;;; Distributed under the GNU GPL2 <http://www.gnu.org/copyleft/gpl.html>:
 ;;; No warranty; you may copy/modify/redistribute under the same
 ;;; conditions with the source code.
 
 ;; for your cut&paste convenience:
-;; (load "/usr/src/clisp/modules/postgresql/sql.lisp")
-;; (cl-user::sql-test-1)
+;; (load "postgresql/sql.lisp")
+;; (sql-demo::sql-test-1)
 
-(in-package :cl-user)
+(require "postgresql")
+
+(defpackage "SQL-DEMO"
+  (:use "CL" "EXT" "FFI" "SQL"))
+
+(in-package "SQL-DEMO")
 
 ;;;
 ;;; Helper Functions
@@ -26,21 +31,21 @@
 
 (defun sql-error (conn res &rest mesgs)
   ;; if you do `PQfinish' twice on the same object, you will get segfault!
-  (when conn (sql:PQfinish conn))
+  (when conn (sql::PQfinish conn))
   ;; if you do `PQclear' twice on the same object, you will get segfault!
-  (when res (sql:PQclear res))
+  (when res (sql::PQclear res))
   (error 'sql-error :mesg (apply #'concatenate 'string mesgs)
                     :type (if res :request :connection)))
 
 (defun sql-connect (host port opts tty name login passwd)
-  (let ((conn (sql:PQsetdbLogin host port opts tty name login passwd)))
-    (if (= (sql:PQstatus conn) sql::CONNECTION_OK)
+  (let ((conn (sql::PQsetdbLogin host port opts tty name login passwd)))
+    (if (= (sql::PQstatus conn) sql::CONNECTION_OK)
         (format *sql-log* "~&Connection OK:~% db name: `~a'
  host:port[tty]: ~a:~a[~a]~% options: `~a'~%"
-                (sql:PQdb conn) (sql:PQhost conn) (sql:PQport conn)
-                (sql:PQtty conn) (sql:PQoptions conn))
+                (sql::PQdb conn) (sql::PQhost conn) (sql::PQport conn)
+                (sql::PQtty conn) (sql::PQoptions conn))
         (sql-error conn nil "PQconnectdb/template1: "
-                   (sql:PQerrorMessage conn)))
+                   (sql::PQerrorMessage conn)))
     conn))
 
 (defmacro with-sql-connection ((conn host port opts tty name login passwd)
@@ -48,13 +53,13 @@
   `(let ((,conn (sql-connect ,host ,port ,opts ,tty ,name ,login ,passwd)))
     (unwind-protect (progn ,@body)
       ;; close the connection to the database and cleanup
-      (when ,conn (sql:PQfinish ,conn)))))
+      (when ,conn (sql::PQfinish ,conn)))))
 
 (defun sql-transaction (conn command status &optional clear-p)
-  (let ((res (sql:PQexec conn command)))
-    (unless (= status (sql:PQresultStatus res))
-      (sql-error conn res command ": " (sql:PQresultErrorMessage res)))
-    (when clear-p (sql:PQclear res))
+  (let ((res (sql::PQexec conn command)))
+    (unless (= status (sql::PQresultStatus res))
+      (sql-error conn res command ": " (sql::PQresultErrorMessage res)))
+    (when clear-p (sql::PQclear res))
     (format *sql-log* " * OK: ~a~%" command)
     res))
 
@@ -62,7 +67,7 @@
   `(let ((,res (sql-transaction ,conn ,command ,status)))
     (unwind-protect (progn ,@body)
       ;; avoid memory leaks
-      (when ,res (sql:PQclear ,res)))))
+      (when ,res (sql::PQclear ,res)))))
 
 ;;;
 ;;; Simple Test
@@ -79,18 +84,18 @@
     ;; FETCH ALL
     (with-sql-transaction (res conn "FETCH ALL in mycursor"
                                sql::PGRES_TUPLES_OK)
-      (let ((nfields (sql:PQnfields res))
-            (ntuples (sql:PQntuples res)))
+      (let ((nfields (sql::PQnfields res))
+            (ntuples (sql::PQntuples res)))
         (format t " + ~d fields; ~d ntuples~%" nfields ntuples)
 
         ;; first, print out the attribute names
         (dotimes (ii nfields (format t "~2%"))
-          (format t "~15s" (sql:PQfname res ii)))
+          (format t "~15s" (sql::PQfname res ii)))
 
         ;; next, print out the instances
-        (dotimes (ii (sql:PQntuples res))
+        (dotimes (ii (sql::PQntuples res))
           (dotimes (jj nfields (terpri))
-            (format t "~15s" (sql:PQgetvalue res ii jj))))))
+            (format t "~15s" (sql::PQgetvalue res ii jj))))))
 
     ;; close the cursor
     (sql-transaction conn "CLOSE mycursor" sql::PGRES_COMMAND_OK t)
@@ -118,8 +123,8 @@
   (with-sql-connection (conn nil nil nil nil (sys::getenv "USER") nil nil)
     (sql-transaction conn "LISTEN TBL2" sql::PGRES_COMMAND_OK t)
 
-    (loop (sql:PQconsumeInput conn)
-          (loop :for notify = (sql:PQnotifies conn)
+    (loop (sql::PQconsumeInput conn)
+          (loop :for notify = (sql::PQnotifies conn)
                 :while (ffi:validp notify) :do
                 ;; unfortunately, (FFI:VALIDP #<FOREIGN-ADDRESS #x00000000>)
                 ;; ==> T, so this won't work!
@@ -166,20 +171,20 @@
 
     (with-sql-transaction (res conn "FETCH ALL in mycursor"
                                sql::PGRES_TUPLES_OK)
-      (let ((i-fnum (sql:PQfnumber res "i"))
-            (d-fnum (sql:PQfnumber res "d"))
-            (p-fnum (sql:PQfnumber res "p"))
-            (nfields (sql:PQnfields res))
-            (ntuples (sql:PQntuples res)))
+      (let ((i-fnum (sql::PQfnumber res "i"))
+            (d-fnum (sql::PQfnumber res "d"))
+            (p-fnum (sql::PQfnumber res "p"))
+            (nfields (sql::PQnfields res))
+            (ntuples (sql::PQntuples res)))
         (format t " + ~d fields; ~d ntuples; i: ~d; d: ~d; p: ~d~%"
                 nfields ntuples i-fnum d-fnum p-fnum)
         (dotimes (ii 3)
           (format t "type[~d] = ~d, size[~d] = ~d~%"
-                  ii (sql:PQftype res ii) ii (sql:PQfsize res ii)))
+                  ii (sql::PQftype res ii) ii (sql::PQfsize res ii)))
         (dotimes (ii ntuples)
-          (let ((plen (sql:PQgetlength res ii p-fnum))
-                (ival (sql:PQgetvalue res ii i-fnum))
-                (dval (sql:PQgetvalue res ii d-fnum)))
+          (let ((plen (sql::PQgetlength res ii p-fnum))
+                (ival (sql::PQgetvalue res ii i-fnum))
+                (dval (sql::PQgetvalue res ii d-fnum)))
             (format t " ++ plen: ~d; ival: ~d; dval: ~f~%" plen ival dval)))))
 
     (sql-transaction conn "CLOSE mycursor" sql::PGRES_COMMAND_OK t)
