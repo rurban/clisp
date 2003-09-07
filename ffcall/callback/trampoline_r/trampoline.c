f3883027278ffeecefcdcde6dd3edcacea9122a1
haible <unknown>
2003-09-07 17:42:07 +0000

    Assume that <stdlib.h> declares malloc().

--- ffcall/callback/trampoline_r/trampoline.c
+++ ffcall/callback/trampoline_r/trampoline.c
@@ -95,20 +95,9 @@ extern void (*tramp_r) (); /* trampoline prototype */
 #include <stdio.h> /* declares fprintf() */
 
 #include <sys/types.h>
-#include <stdlib.h> /* declares abort(), free() */
+#include <stdlib.h> /* declares abort(), malloc(), free() */
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
-#endif
-
-/* Declare malloc(). */
-#ifndef malloc
-#ifdef __cplusplus
-extern "C" RETMALLOCTYPE malloc (MALLOC_SIZE_T size);
-#elif defined(__STDC__)
-extern RETMALLOCTYPE malloc (MALLOC_SIZE_T size);
-#else
-extern RETMALLOCTYPE malloc();
-#endif
 #endif
 
 /* Declare getpagesize(). */
