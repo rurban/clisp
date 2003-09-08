3cf1f9759a9410131a77ec0ac9618891132103db
Bruno Haible <bruno@clisp.org>
2003-09-08 11:06:53 +0000

    Assume that <sys/shm.h> declares shmat() like POSIX specifies it.

--- ffcall/callback/trampoline_r/trampoline.c
+++ ffcall/callback/trampoline_r/trampoline.c
@@ -171,7 +171,6 @@ extern RETGETPAGESIZETYPE getpagesize ();
 #endif
 #ifdef __cplusplus
 extern "C" int shmget (key_t key, SHMGET_SIZE_T size, int shmflg);
-extern "C" RETSHMATTYPE shmat (int shmid, SHMAT_CONST RETSHMATTYPE shmaddr, int shmflg);
 #ifdef SHMCTL_DOTS
 extern "C" int shmctl (int shmid, int cmd, ...);
 #else
@@ -179,7 +178,6 @@ extern "C" int shmctl (int shmid, int cmd, struct shmid_ds * buf);
 #endif
 #elif defined(__STDC__)
 extern int shmget (key_t key, SHMGET_SIZE_T size, int shmflg);
-extern RETSHMATTYPE shmat (int shmid, SHMAT_CONST RETSHMATTYPE shmaddr, int shmflg);
 #ifdef SHMCTL_DOTS
 extern int shmctl (int shmid, int cmd, ...);
 #else
@@ -187,7 +185,6 @@ extern int shmctl (int shmid, int cmd, struct shmid_ds * buf);
 #endif
 #else
 extern int shmget ();
-extern RETSHMATTYPE shmat ();
 extern int shmctl ();
 #endif
 #endif
