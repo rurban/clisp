b8b073dd82b4761ad0bdd381f0fe94648adc6049
sds <sds@podval.org>
2003-10-16 21:36:24 +0000

    (ExitWindowsEx, GetGuiResources, GetConsoleTitleA, SetConsoleTitleA): added

--- modules/bindings/win32/win32.lisp
+++ modules/bindings/win32/win32.lisp
@@ -23,6 +23,7 @@
 (in-package "W32")
 
 (def-c-type handle c-pointer)
+(def-c-type dword uint32)
 
 ;; this is not necessary: we are not creating a C file anyway
 ;;(c-lines "#define WINVER 0x0500~%#include <windows.h>~%")
@@ -31,7 +32,7 @@
   (:arguments) (:return-type c-string))
 
 (def-call-out GetLastError (:library "kernel32.dll")
-  (:arguments) (:return-type uint32))
+  (:arguments) (:return-type dword))
 
 (def-call-out GetCurrentProcess (:library "kernel32.dll")
   (:arguments) (:return-type handle))
@@ -40,12 +41,12 @@
   (:arguments) (:return-type handle))
 
 (def-call-out GetCurrentProcessId (:library "kernel32.dll")
-  (:arguments) (:return-type uint32))
+  (:arguments) (:return-type dword))
 
 (def-call-out CloseHandle (:library "kernel32.dll")
   (:arguments (handle handle)) (:return-type boolean))
 
-;; (c-lines "<winnt.h>~%")
+;; (c-lines "#include <winnt.h>~%")
 (eval-when (compile eval load)
 (def-c-enum RIGHTS
   (SYNCHRONIZE              #x100000)
@@ -73,13 +74,31 @@
    #.(cl:logior STANDARD_RIGHTS_REQUIRED SYNCHRONIZE #xFFF)))
 
 (def-call-out OpenProcess (:library "kernel32.dll")
-  (:arguments (access-flag uint32) ; see PROCESS above
+  (:arguments (access-flag dword) ; PROCESS
               (inherit-handle boolean)
-              (pid uint32))
+              (pid dword))
   (:return-type handle))
 
+;;(c-lines "#include <winuser.h>~%")
+(def-c-enum EWX                 ; shutdown operation
+  (EWX_LOGOFF           #x0)
+  (EWX_SHUTDOWN         #x1)
+  (EWX_REBOOT           #x2)
+  (EWX_FORCE            #x4)
+  (EWX_POWEROFF         #x8)
+  (EWX_FORCEIFHUNG     #x10))
+(def-call-out ExitWindowsEx (:library "User32.dll")
+  (:arguments (flags uint)      ; EWX
+              (reserved dword))
+  (:return-type boolean))
+
+(def-c-enum GR_OBJECTS GR_GDIOBJECTS GR_USEROBJECTS)
+(def-call-out GetGuiResources (:library "User32.dll")
+  (:arguments (process handle)
+              (flags dword))    ; GR_OBJECTS
+  (:return-type dword))
+
 ;; create an icon
-;;(c-lines "<winuser.h>~%")
 (def-c-enum image_type
   (IMAGE_BITMAP 0) IMAGE_ICON IMAGE_CURSOR IMAGE_ENHMETAFILE)
 (def-c-enum load_options
@@ -99,7 +118,7 @@
   (:arguments (application-instance-handle handle)
               (image-name c-string)
               (type uint) (width int) (height int)
-              (options int))
+              (options int))    ; load_options
   (:return-type handle))
 #| example:
  (setq icon (win32:LoadImageA nil "d:\\gnu\\clisp\\current\\doc\\clisp.ico"
@@ -107,17 +126,29 @@
 |#
 
 (eval-when (compile eval load)
-  (defconstant MAX_PATH 260))   ; see <windef.h>
+  (defconstant BUFSIZ 4096)     ; <stdio.h>
+  (defconstant MAX_PATH 260))   ; <windef.h>
 
 (def-call-out GetModuleFileNameA (:library "kernel32.dll")
   (:arguments (application-instance-handle handle)
               (name (c-ptr (c-array-max character #.MAX_PATH)) :out :alloca)
-              (size int))  ; always pass MAX_PATH as the second argument
-  (:return-type uint32))
+              (size dword))  ; always pass MAX_PATH as the second argument
+  (:return-type dword))
 
 (def-call-out GetModuleHandleA (:library "kernel32.dll")
   (:arguments (name c-string))
   (:return-type handle))
+
+;; (c-lines "#include <winicon.h>~%")
+
+(def-call-out GetConsoleTitleA (:library "kernel32.dll")
+  (:arguments (buffer (c-ptr (c-array-max character #.BUFSIZ)) :out :alloca)
+              (size dword))  ; always pass BUFSIZ as the only argument
+  (:return-type dword))
+
+(def-call-out SetConsoleTitleA (:library "kernel32.dll")
+  (:arguments (title c-string))
+  (:return-type boolean))
 
 ;;; ==========================================================================
 ;;; clean up
