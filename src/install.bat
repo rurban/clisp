83e17650f0d0b96b4f643206df03471f6ec0b0a7
sds <unknown>
2004-01-01 18:50:47 +0000

    fixed bug #[ 868881 ]: replaced `|' with `I' and `/' with `\' for WinXP

--- src/install.bat
+++ src/install.bat
@@ -1,21 +1,21 @@
 @echo off
 
-echo +==========================================================+
-echo | this will install CLISP on your system,                  |
-echo | associating file types FAS, MEM and LISP with CLISP.     |
-echo | it will also create a shortcut to CLISP on your desktop. |
-echo |               press C-c to abort                         |
-echo +==========================================================+
+echo +----------------------------------------------------------+
+echo I this will install CLISP on your system,                  I
+echo I associating file types FAS, MEM and LISP with CLISP.     I
+echo I it will also create a shortcut to CLISP on your desktop. I
+echo I               press C-c to abort                         I
+echo +----------------------------------------------------------+
 pause
 
 if exist src\install.lisp goto installsrc
 if exist install.lisp goto install
 goto notfound
 :installsrc
-full/lisp.exe -B . -M full/lispinit.mem -norc -C src\install.lisp
+full\lisp.exe -B . -M full\lispinit.mem -norc -C src\install.lisp
 goto exit
 :install
-full/lisp.exe -B . -M full/lispinit.mem -norc -C install.lisp
+full\lisp.exe -B . -M full\lispinit.mem -norc -C install.lisp
 goto exit
 :notfound
 echo Sorry, install.lisp not found, cannot install
