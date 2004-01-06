11d82f549e6b69bb936d9734d0a343582af7b472
sds <unknown>
2004-01-06 19:51:23 +0000

    (with-standard-io-syntax): bind *PRINT-SYMBOLS-LONG*

--- src/defs2.lisp
+++ src/defs2.lisp
@@ -326,6 +326,7 @@
            (SYSTEM::*PRIN-STREAM*       NIL) ; CLISP specific
            (SYSTEM::*PRIN-LINELENGTH*   79)  ; CLISP specific
            (SYSTEM::*PRIN-LINE-PREFIX*  NIL) ; CLISP specific
+           (CUSTOM::*PRINT-SYMBOLS-LONG* NIL) ; CLISP specific
            ;; reader variables:
            (*READ-BASE*                 10)
            (*READ-DEFAULT-FLOAT-FORMAT* 'SINGLE-FLOAT)
