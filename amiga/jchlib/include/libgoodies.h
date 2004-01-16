21bb07e84261fc7c6faba143328e79f43778bfa7
haible <unknown>
2004-01-16 11:04:48 +0000

    Remove AmigaOS support.

--- amiga/jchlib/include/libgoodies.h
+++
@@ -1,6 +0,0 @@
-/* Tiny GCC Library
- * libgoodies.h
- * Jörg Höhle, 19-Sept-94
- */
-LONG BPTRfprintf(BPTR, char *, ...);
-LONG setmode(BPTR, long);	/* SetMode() for 2.0 and below */
--- amiga/jchlib/startup/def_window.c
+++
@@ -1,6 +0,0 @@
-#include <exec/types.h>
-
-#ifdef SUPPORT_1_3
-UBYTE _WDefName13[] = "CON:0/11/640/186/Program-console";
-#endif
-UBYTE _WDefName[] =  "CON:0/11//186/Program-console/CLOSE/AUTO/WAIT";
