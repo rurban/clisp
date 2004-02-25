4ddbe9e2a0039b40a0b05aaa7ed970184edd77e2
Bruno Haible <bruno@clisp.org>
2004-02-25 16:23:00 +0000

    Use the CPPFLAGS variable, in the same sense as autoconf uses it.

--- modules/fastcgi/link.sh
+++ modules/fastcgi/link.sh
@@ -1,6 +1,8 @@
 # link.sh for FastCGI library
 # $Id$
-make clisp-module CC="${CC}" CFLAGS="${CFLAGS}" INCLUDES="$absolute_linkkitdir"
+make clisp-module \
+     CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" \
+     INCLUDES="$absolute_linkkitdir"
 NEW_FILES="fastcgi.o fastcgi_wrappers.o"
 NEW_LIBS="fastcgi.o fastcgi_wrappers.o -lfcgi"
 NEW_MODULES="fastcgi"
--- modules/oracle/link.sh
+++ modules/oracle/link.sh
@@ -3,7 +3,9 @@
 #
 # $Id$
 
-make clisp-module CC="${CC}" CFLAGS="${CFLAGS}" INCLUDES="$absolute_linkkitdir"
+make clisp-module \
+     CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" \
+     INCLUDES="$absolute_linkkitdir"
 
 NEW_FILES="oracle.o oiface.o orafns.o"
 
