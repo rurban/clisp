0c673df7963377f16d2394a70542e69e83b978db
haible <unknown>
2004-03-01 11:12:07 +0000

    Fix a possible compilation problem with Solaris and LFS.

--- src/spvwtabs.d
+++ src/spvwtabs.d
@@ -1,7 +1,7 @@
 /*
  * Moved out of memory management:
  * table of all fixed symbols
- * Bruno Haible 1990-2001
+ * Bruno Haible 1990-2004
  */
 
 #include "lispbibl.c"
@@ -15,6 +15,11 @@
 #undef write
 /* LISPBIBL.D does "#define export" */
 #undef export
+/* Large File Support on some versions of Solaris does "#define open open64",
+   "#define truncate truncate64", "#define ftruncate ftruncate64" */
+#undef open
+#undef truncate
+#undef ftruncate
 
 /* Table of all fixed symbols: */
 global struct symbol_tab_ symbol_tab_data
