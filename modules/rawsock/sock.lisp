42a06b97debf5c102f8645422571dab5ddf53e2b
sds <unknown>
2004-04-23 00:38:37 +0000

    implemented the remaining raw socket functions

--- modules/rawsock/sock.lisp
+++ modules/rawsock/sock.lisp
@@ -7,25 +7,33 @@
 (defpackage "RAWSOCK"
   (:documentation "Raw Socket access")
   (:use "LISP")
-  (:export "SOCKET" "CLOSESOCK" "BUFFER"
+  (:export "CLOSESOCK" "BUFFER" "OPEN-UNIX-SOCKET"
            "ACCEPT" "BIND" "CONNECT"
-           "GETPEERNAME" "GETSOCKNAME" "GETSOCKOPT"
+           "GETPEERNAME" "GETSOCKNAME"
            "LISTEN" "RECV" "RECVFROM" "RECVMSG"
            "SEND" "SENDMSG" "SENDTO"
-           "SETSOCKOPT" "SHUTDOWN" "SOCKET" "SOCKETPAIR"
-           "SOCKADDR" "MAKE-SOCKADDR" "MSGHDR" "MAKE-MSGHDR"
-           "CMSGHDR" "MAKE-CMSGHDR" "LINGER" "MAKE-LINGER"
+           "SHUTDOWN" "SOCKET" "SOCKETPAIR"
+           "SOCK-READ" "SOCK-WRITE" "POLL"
+           "SOCKADDR" "MAKE-SOCKADDR" "SOCKADDR-FAMILY" "MSGHDR" "MAKE-MSGHDR"
            "CONFIGDEV" "IPCSUM" "ICMPCSUM" "TCPCSUM" "UDPCSUM"))
 
 (IN-PACKAGE "RAWSOCK")
 (PUSHNEW :RAWSOCK *FEATURES*)
 (PUSH "RAWSOCK" EXT:*SYSTEM-PACKAGE-LIST*)
 
-(defstruct (sockaddr (:constructor make-sockaddr (data)))
-  (data (error) :read-only t :type (vector (unsigned-byte 8))))
+(macrolet ((missing (type)
+             `(error "~S: missing ~S slot" ',type 'data)))
+(defstruct (sockaddr (:constructor make-sa (data)))
+  (data (missing sockaddr) :read-only t :type (vector (unsigned-byte 8))))
 (defstruct (msghdr (:constructor make-msghdr (data)))
-  (data (error) :read-only t :type (vector (unsigned-byte 8))))
-(defstruct (cmsghdr (:constructor make-cmsghdr (data)))
-  (data (error) :read-only t :type (vector (unsigned-byte 8))))
-(defstruct (linger (:constructor make-linger (data)))
-  (data (error) :read-only t :type (vector (unsigned-byte 8))))
+  (data (missing msghdr) :read-only t :type (vector (unsigned-byte 8))))
+)
+
+(defun open-unix-socket (pathname &optional (type :SOCK_STREAM))
+  (let* ((socket (socket :AF_UNIX type 0))
+         (address (make-sockaddr :AF_UNIX
+                                 (ext:convert-string-to-bytes
+                                  (namestring pathname)
+                                  ext:*pathname-encoding*))))
+    (connect socket address)
+    (values socket address)))
