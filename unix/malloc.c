2576bcb707af3cc1f6a4d3d8488d44c2748101ea
sds <unknown>
2004-05-16 17:14:54 +0000

    align output, and, when no arguments give, try the whole range

--- unix/malloc.c
+++ unix/malloc.c
@@ -5,23 +5,27 @@
 */
 #include <stdio.h>
 
-void printf_address (addr)
-  unsigned long addr;
-{ if (sizeof(unsigned long) <= 4)
-    printf ("#x%8X", (unsigned int)addr);
+void printf_address (unsigned long addr) {
+  if (sizeof(unsigned long) <= 4)
+    printf ("#x%08X", (unsigned int)addr);
   else
-    printf ("#x%8X%08X",(unsigned int)(addr>>32),(unsigned int)(addr&0xFFFFFFFF));
+    printf ("#x%08X%08X",(unsigned int)(addr>>32),
+            (unsigned int)(addr & 0xFFFFFFFF));
 }
 
-int main (argc,argv)
-  int argc;
-  char** argv;
-{ int i;
-  for (i = 1; i < argc; i++)
-    { int arg = atoi(argv[i]);
-      unsigned long result = (unsigned long)malloc(arg);
-      printf ("malloc(%d) = ",arg); printf_address (result); printf ("\n");
-    }
+void test_malloc (unsigned long size) {
+  unsigned long result = (unsigned long)malloc(size);
+  printf ("malloc(%12d) = ",size); printf_address (result); printf ("\n");
+}
+
+int main (int argc, char* argv[]) {
+  int i;
+  if (argc == 1)
+    for (i = 1; i < 40; i++)
+      test_malloc(1 << i);
+  else
+    for (i = 1; i < argc; i++)
+      test_malloc(atol(argv[i]));
   printf ("&main = "); printf_address ((unsigned long)&main); printf ("\n");
-  exit(0);
+  return 0;
 }
