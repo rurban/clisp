337fdc78e840a59367adb0af5a9e0b384bf71f33
Sam Steingold <sds@gnu.org>
2004-05-28 14:06:07 +0000

    (d-font-lock-extra-types): added "\\sw+_Pseudofun"

--- emacs/d-mode.el
+++ emacs/d-mode.el
@@ -152,7 +152,8 @@ The point should be on the prototype and the definition should follow."
 (defvar d-font-lock-extra-types
   '(nconc (list "bool" "object" "chart" "[otac]int" "signean" "\\sw+_T"
            "s[aco]int" "[csu]?int[BCDLPQWX0-9]*" "hfint" "fcint" "SPint"
-            "[SU]LONG" "[SU]BYTE" "[DSU]WORD" "[SU]LONGLONG" "RET\\sw*TYPE"
+           "[SU]LONG" "[SU]BYTE" "[DSU]WORD" "[SU]LONGLONG" "RET\\sw*TYPE"
+           "\\sw+_Pseudofun"
            "Values" "SOCKET" "Handle" "stringarg" "FILETIME")
     c-font-lock-extra-types)
   "Extra types to be fontified as such.")
