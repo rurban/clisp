cad579a7693e908bf51b44f033bf3b229691e894
haible <unknown>
2004-06-11 12:06:38 +0000

    Reorder includes.

--- src/clos.lisp
+++ src/clos.lisp
@@ -15,26 +15,29 @@
 ;; em = effective method
 
 (load "clos-class1")
+; Now DEFCLASS works (except for accessor methods).
 (load "clos-slots1")
 (load "clos-class2")
 (load "clos-method1")
 (load "clos-methcomb1")
-(load "clos-method2")
 (load "clos-genfun1")
 (load "clos-methcomb2")
 (load "clos-genfun2")
 (load "clos-methcomb3")
-(load "clos-genfun3")
 (load "clos-methcomb4")
+(load "clos-methcomb5")
+(load "clos-method2")
+(load "clos-genfun3")
 (load "clos-genfun4")
+; Now DEFGENERIC, DEFMETHOD work. DEFCLASS works fully.
 (load "clos-class3")
 (load "clos-genfun5")
 (load "clos-method3")
 (load "clos-slots2")
 (load "clos-class5")
 (load "clos-slotdef2")
+; Now instance creation works. Instances can be passed to generic functions.
 (setq *classes-finished* t)
 (load "clos-slotdef3")
 (load "clos-print")
 (load "documentation")
-(load "clos-methcomb5")
