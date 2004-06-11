b44a4bc5ab8f3760bb0400849c0763ae879fd021
haible <unknown>
2004-06-11 12:16:23 +0000

    Move code around, from clos-methcomb[345].lisp to clos-methcomb2.lisp.

--- src/clos-genfun1.lisp
+++ src/clos-genfun1.lisp
@@ -5,33 +5,3 @@
 
 (in-package "CLOS")
 
-
-;; CLtL2 28.1.6.2., ANSI CL 7.6.2. Applicable methods
-(defun method-applicable-p (method required-arguments)
-  (every #'typep required-arguments
-         (std-method-parameter-specializers method)))
-
-;; CLtL2 28.1.7.1., ANSI CL 7.6.6.1. Sorting the applicable methods by
-;;                                   precedence order
-(defun sort-applicable-methods (methods required-arguments argument-order)
-  (sort (copy-list methods)
-        #'(lambda (method1 method2) ; method1 < method2 ?
-            (let ((specializers1 (std-method-parameter-specializers method1))
-                  (specializers2 (std-method-parameter-specializers method2)))
-              (dolist (arg-index argument-order nil)
-                (let ((arg (nth arg-index required-arguments))
-                      (psp1 (nth arg-index specializers1))
-                      (psp2 (nth arg-index specializers2)))
-                  (if (consp psp1)
-                    (if (consp psp2)
-                      nil         ; (EQL x) = (EQL x)
-                      (return t)) ; (EQL x) < <class>  ==>  method1 < method2
-                    (if (consp psp2)
-                      (return nil) ; <class> > (EQL x)   ==>  method1 > method2
-                      ;; two classes: compare the position in the CPL of arg:
-                      (let* ((cpl (class-precedence-list (class-of arg)))
-                             (pos1 (position psp1 cpl))
-                             (pos2 (position psp2 cpl)))
-                        (cond ((< pos1 pos2) (return t)) ; method1 < method2
-                              ((> pos1 pos2) (return nil)) ; method1 > method2
-                              ))))))))))
