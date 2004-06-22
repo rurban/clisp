0a438b070f69bc60984e0394900c003252ade130
haible <unknown>
2004-06-22 12:37:31 +0000

    Variable <method>.

--- src/clos-method1.lisp
+++ src/clos-method1.lisp
@@ -8,6 +8,8 @@
 
 (defstruct (method (:predicate nil) (:copier nil) (:constructor nil)))
 
+(defparameter <method> (find-class 'method))
+
 (defstruct (standard-method (:include method) (:conc-name "STD-METHOD-")
                             (:copier nil)) ; (:print-object print-std-method)
   function               ; the function
