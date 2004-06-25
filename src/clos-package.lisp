a53b646ca0226772d8cef677d93c46427321ad3d
haible <bruno@clisp.org>
2004-06-25 10:46:14 +0000

    Implement find-method-combination.

--- src/clos-package.lisp
+++ src/clos-package.lisp
@@ -132,6 +132,7 @@
           ;; MOP for methods
           accessor-method-slot-definition
           ;; MOP for method combinations
+          find-method-combination
           ;; MOP for generic functions
           ;; CLISP specific symbols
           generic-flet generic-labels no-primary-method
