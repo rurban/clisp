3ca0292091d835c59e4f4d03299d54348240b065
haible <unknown>
2004-07-11 13:35:41 +0000

    New function compute-effective-slot-definition.

--- src/clos-package.lisp
+++ src/clos-package.lisp
@@ -125,7 +125,7 @@
           class-direct-subclasses class-direct-slots class-slots
           class-direct-default-initargs class-default-initargs class-prototype
           class-finalized-p finalize-inheritance
-          compute-class-precedence-list
+          compute-class-precedence-list compute-effective-slot-definition
           validate-superclass add-direct-subclass remove-direct-subclass
           ;; MOP for specializers
           specializer-direct-generic-functions specializer-direct-methods
