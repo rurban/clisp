137df7cbe32705dcfa72bf65ac54a52bf7d50981
Bruno Haible <bruno@clisp.org>
2004-07-26 08:59:34 +0000

    Make METHOD objects STANDARD-OBJECT instances.

--- src/clos-method3.lisp
+++ src/clos-method3.lisp
@@ -1,9 +1,42 @@
-;;;; Common Lisp Object System for CLISP: Methods
+;;;; Common Lisp Object System for CLISP
+;;;; Methods
+;;;; Part n-2: make/initialize-instance methods, generic functions.
 ;;;; Bruno Haible 21.8.1993 - 2004
 ;;;; Sam Steingold 1998 - 2004
 ;;;; German comments translated into English: Stefan Kain 2002-04-08
 
 (in-package "CLOS")
+
+
+;;; Lift the initialization protocol.
+
+(defmethod initialize-instance ((method standard-method) &rest args
+                                &key qualifiers
+                                     ;lambda-list
+                                     ;specializers
+                                     function
+                                     ;documentation
+                                     initfunction
+                                     wants-next-method-p
+                                     parameter-specializers
+                                     signature
+                                     gf
+                                     origin
+                                &allow-other-keys)
+  (declare (ignore qualifiers function initfunction wants-next-method-p
+                   parameter-specializers signature gf origin))
+  (apply #'initialize-instance-<standard-method> method args))
+
+(defmethod initialize-instance ((method standard-accessor-method) &rest args
+                                &key slot-definition
+                                &allow-other-keys)
+  (declare (ignore slot-definition))
+  (apply #'initialize-instance-<standard-accessor-method> method args))
+
+(defmethod reinitialize-instance ((instance method) &rest initargs)
+  (declare (ignore initargs))
+  (error (TEXT "~S: The MOP does not allow reinitializing ~S")
+         'reinitialize-instance instance))
 
 
 (defgeneric method-qualifiers (method)
