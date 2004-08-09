331736680a9d96b2458c62b096126158db28c683
Sam Steingold <sds@gnu.org>
2004-08-09 15:19:22 +0000

    (expand-loop): given CLtL2 syntax removed by ANSI, either warn or signal an error, depending on *LOOP-ANSI*

--- src/spvw.d
+++ src/spvw.d
@@ -1317,6 +1317,7 @@ local void init_symbol_values (void) {
   define_variable(S(load_print),NIL); /* *LOAD-PRINT* := NIL */
   define_variable(S(compile_print),NIL); /* *COMPILE-PRINT* := NIL */
   define_variable(S(compile_verbose),T); /* *COMPILE-VERBOSE* := T */
+  define_variable(S(loop_ansi),NIL); /* CUSTOM:*LOOP-ANSI* := NIL */
   define_variable(S(defun_accept_spelalist),NIL); /* CUSTOM:*DEFUN-ACCEPT-SPECIALIZED-LAMBDA-LIST* := NIL */
   # for FOREIGN:
  #ifdef DYNAMIC_FFI
