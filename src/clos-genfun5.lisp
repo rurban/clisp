ca18ce8ed6a2552aa4e391b44f1801677797b2f8
Bruno Haible <bruno@clisp.org>
2004-09-01 10:33:51 +0000

    Use ensure-generic-function instead of do-defgeneric.

--- src/clos-genfun5.lisp
+++ src/clos-genfun5.lisp
@@ -18,19 +18,19 @@
 ;;; Generic function definition customization
 
 ;; MOP p. 50
-(fmakunbound 'ensure-generic-function-using-class)
-(defgeneric ensure-generic-function-using-class (gf funname
-                                                 &key generic-function-class
-                                                      lambda-list
-                                                      argument-precedence-order
-                                                      method-class
-                                                      method-combination
-                                                      documentation
-                                                      declarations
-                                                      declare
-                                                      environment
-                                                 &allow-other-keys)
-  (:method ((gf generic-function) funname &rest args)
-    (apply #'ensure-generic-function-using-class-<t> gf funname args))
-  (:method ((gf null) funname &rest args)
-    (apply #'ensure-generic-function-using-class-<t> gf funname args)))
+(let ((*allow-making-generic* t))
+  (defgeneric ensure-generic-function-using-class (gf funname
+                                                   &key generic-function-class
+                                                        lambda-list
+                                                        argument-precedence-order
+                                                        method-class
+                                                        method-combination
+                                                        documentation
+                                                        declarations
+                                                        declare
+                                                        environment
+                                                   &allow-other-keys)
+    (:method ((gf generic-function) funname &rest args)
+      (apply #'ensure-generic-function-using-class-<t> gf funname args))
+    (:method ((gf null) funname &rest args)
+      (apply #'ensure-generic-function-using-class-<t> gf funname args))))
