a99972815400589e3d3c901d7980e0c4af5af015
sds <sds@podval.org>
2004-10-11 17:05:29 +0000

    (MODULE-INFO): return the DLL information for :FFI argument (instead of 0)  all info when VERBOSE, names only otherwise

--- src/misc.d
+++ src/misc.d
@@ -607,15 +607,23 @@ LISPFUNN(set_ansi,1)
 LISPFUN(module_info,seclass_no_se,0,2,norest,nokey,0,NIL)
 { /* (EXT:MODULE-INFO)       ==> list of all module names
   (EXT:MODULE-INFO "name")   ==> "name", subr-count, obj-count
-  (EXT:MODULE-INFO "name" t) ==> "name", s-count, s-list, o-count, o-list */
+  (EXT:MODULE-INFO "name" t) ==> "name", s-count, s-list, o-count, o-list
+  (EXT:MODULE-INFO :ffi)     ==> shared library names
+  (EXT:MODULE-INFO :ffi t)   ==> DLLs & all associated objects */
   var object verbose = popSTACK();
   var bool verbosep = !missingp(verbose);
   var object arg = popSTACK();
   if (missingp(arg))
     VALUES1(listof(modules_names_to_stack()));
  #if defined(DYNAMIC_FFI) && (defined(WIN32_NATIVE) || defined(HAVE_DLOPEN))
-  else if (eq(arg,Fixnum_0)) {
-    pushSTACK(O(foreign_libraries)); funcall(L(copy_tree),1);
+  else if (eq(arg,S(Kffi))) {
+    if (!verbosep) {            /* just the library names */
+      pushSTACK(L(car)); pushSTACK(O(foreign_libraries));
+      funcall(L(mapcar),2);
+    } else {                      /* everything */
+      pushSTACK(O(foreign_libraries));
+      funcall(L(copy_tree),1);
+    }
   }
  #endif
   else {
