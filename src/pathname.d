73a85eb084ada606197db525daf81dffd5491b4e
haible <unknown>
2005-01-28 14:02:18 +0000

    Fix (logical-pathname ":").

--- src/pathname.d
+++ src/pathname.d
@@ -1169,9 +1169,10 @@ local maygc object parse_logical_host_prefix (zustand* zp, object string) {
     var uintL len = zp->index - startindex;
     pushSTACK(string);
     host = allocate_string(len);
+    string = popSTACK();
     /* and fill it: */
     if (len > 0)
-      copy_upcase(host,0,popSTACK()/* ==string */,startindex,len);
+      copy_upcase(host,0,string,startindex,len);
   }
   /* skip ':' */
   Z_SHIFT(*zp,1);
