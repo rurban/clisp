8cab1590cf605ac078ce7e83850483ddd1e46058
Sam Steingold <sds@gnu.org>
2005-02-17 15:51:37 +0000

    (__EXTENSIONS__) [__sun]: define to enable confstr() prototype and avoid a gcc warning

--- modules/syscalls/calls.c
+++ modules/syscalls/calls.c
@@ -31,6 +31,9 @@
 # endif
 #endif
 #if defined(HAVE_UNISTD_H)
+# if defined(__sun)
+#  define __EXTENSIONS__        /* enable confstr() prototype */
+# endif
 # include <unistd.h>
 #endif
 #if defined(HAVE_SYS_UNISTD_H)
