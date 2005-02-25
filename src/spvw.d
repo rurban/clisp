4d9d2960081df2ce8f671b68745c08092b483843
Sam Steingold <sds@gnu.org>
2005-02-25 03:29:05 +0000

    (init_lowest_level): predicate find_executable() on HAVE_DISASSEMBLER

--- src/spvw.d
+++ src/spvw.d
@@ -1799,9 +1799,11 @@ local inline void init_lowest_level (char* argv[]) {
  #ifdef WIN32_NATIVE
   init_win32();
  #endif
+ #ifdef HAVE_DISASSEMBLER
   begin_system_call();
   find_executable(argv[0]);
   end_system_call();
+ #endif
 }
 
 # Very late de-initializations,
