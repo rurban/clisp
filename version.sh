7000a0b9bac6cc720795aa99e087a63dd72045e2
sds <unknown>
2005-03-17 21:42:34 +0000

    bumped for yet another woe32/cygwin pre-test

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=2.33.82
-RELEASE_DATE=2005-03-10      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=2.33.83
+RELEASE_DATE=2005-03-14      # in "date +%Y-%m-%d" format
