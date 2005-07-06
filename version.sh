7f1681d38fc24523abc4c10a230fee501d9885ac
sds <unknown>
2005-07-06 14:33:01 +0000

    bumped because of many bug fixes

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=2.33.83
-RELEASE_DATE=2005-03-14      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=2.33.84
+RELEASE_DATE=2005-07-07         # in "date +%Y-%m-%d" format
