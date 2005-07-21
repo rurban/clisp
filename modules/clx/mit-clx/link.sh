8bd75ab86609ac3d7eba0a868659633250e832a7
sds <sds@podval.org>
2005-07-21 13:49:43 +0000

    removed split-sequence

--- modules/clx/mit-clx/link.sh
+++ modules/clx/mit-clx/link.sh
@@ -2,7 +2,7 @@ make clisp-module CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" INCLUDES=
 NEW_FILES=''
 NEW_LIBS=''
 NEW_MODULES=''
-TO_LOAD="package depdefs clx split-sequence dependent macros bufmac buffer display gcontext input requests fonts graphics text attributes translate keysyms manager image resource describe trace shape"
+TO_LOAD="package depdefs clx dependent macros bufmac buffer display gcontext input requests fonts graphics text attributes translate keysyms manager image resource describe trace shape"
 for f in $TO_LOAD; do
   NEW_FILES="$NEW_FILES $f.lisp"
 done
