375abf1c82cdc547e004ad8571dc9c39adfe647a
sds <unknown>
2005-08-18 15:19:27 +0000

    include <time.h> so that "struct timeval" will be available on OSX too

--- modules/rawsock/rawsock.c
+++ modules/rawsock/rawsock.c
@@ -11,6 +11,16 @@
 
 #include "config.h"
 
+#if defined(TIME_WITH_SYS_TIME)
+# include <sys/time.h>
+# include <time.h>
+#else
+# if defined(HAVE_SYS_TIME_H)
+#  include <sys/time.h>
+# elif defined(HAVE_TIME_H)
+#  include <time.h>
+# endif
+#endif
 #if defined(HAVE_SYS_TYPES_H)
 # include <sys/types.h>
 #endif
