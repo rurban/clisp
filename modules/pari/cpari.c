e205583373d941ab3311fd13c135e17a936b0a67
sds <unknown>
2005-08-28 04:05:01 +0000

    moved PARI i/o from Lisp to C to enable C++ compilation ("const" issues)

--- modules/pari/cpari.c
+++ modules/pari/cpari.c
@@ -1,21 +1,55 @@
 /*
  * CLISP interface to PARI <http://pari.math.u-bordeaux.fr/>
  * Copyright (C) 1995 Michael Stoll
- * Copyright (C) 2004 Sam Steingold
+ * Copyright (C) 2004-2005 Sam Steingold
  * This is free software, distributed under the GNU GPL
  */
 
+#include "clisp.h"
+#undef T
 #include <pari/pari.h>
 #include "cpari.h"
 
+/* we could also use DEF-CALL-OUT, but this is faster, smaller,
+   and accommodates the "const" arguments better
+   (CLISP FFI does not produce "const" argument declarations) */
 void clispPutc(char c);
+void clispPutc(char c) {
+  pushSTACK(int_char(c)); pushSTACK(Symbol_value(S(standard_output)));
+  funcall(L(write_char),2);
+}
 void clispPuts(const char *s);
+void clispPuts(const char *s) {
+  pushSTACK(asciz_to_string(s,GLO(foreign_encoding)));
+  pushSTACK(Symbol_value(S(standard_output)));
+  funcall(L(write_string),2);
+}
 void clispFlush(void);
+void clispFlush(void) {
+  pushSTACK(Symbol_value(S(standard_output)));
+  funcall(L(finish_output),1);
+}
 
 void clispErrPutc(char c);
+void clispErrPutc(char c) {
+  pushSTACK(int_char(c)); pushSTACK(Symbol_value(S(error_output)));
+  funcall(L(write_char),2);
+}
 void clispErrPuts(const char *s);
+void clispErrPuts(const char *s) {
+  pushSTACK(asciz_to_string(s,GLO(foreign_encoding)));
+  pushSTACK(Symbol_value(S(error_output)));
+  funcall(L(write_string),2);
+}
 void clispErrFlush(void);
+void clispErrFlush(void) {
+  pushSTACK(Symbol_value(S(error_output)));
+  funcall(L(finish_output),1);
+}
 void clispErrDie(void);
+void clispErrDie(void) {
+  fehler(error,GETTEXT("Internal PARI error."));
+}
 
 PariOUT clispOut = {clispPutc, clispPuts, clispFlush, NULL};
 PariOUT clispErr = {clispErrPutc, clispErrPuts, clispErrFlush, clispErrDie};
