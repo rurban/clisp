3e564d8c5ceefd9d79626a839321910b9d2886c0
sds <unknown>
2005-09-13 17:44:08 +0000

    moved tests from sql.lisp to test.tst

--- modules/postgresql/sql.lisp
+++ modules/postgresql/sql.lisp
@@ -1,14 +1,9 @@
-;;; PostgreSQL higher level functions and demo
-;;; Based on the examples distributed with PostgreSQL (man libpq)
+;;; PostgreSQL higher level functions
 ;;;
 ;;; Copyright (C) 1999-2005 by Sam Steingold
 ;;; Distributed under the GNU GPL2 <http://www.gnu.org/copyleft/gpl.html>:
 ;;; No warranty; you may copy/modify/redistribute under the same
 ;;; conditions with the source code.
-
-;; for your cut&paste convenience:
-;; (load "postgresql/sql.lisp")
-;; (sql::sql-test-1)
 
 (require "postgresql")
 
@@ -87,122 +82,3 @@
 
 ;;; file sql.lisp ends here
 
-;;; tests follow
-#+(or) (progn
-
-;;;
-;;; Simple Test
-;;;
-
-(defun sql-test-1 ()
-  (with-sql-connection (conn :name "template1")
-    (sql-transaction conn "BEGIN" PGRES_COMMAND_OK)
-    ;; fetch instances from the pg_database, the system catalog of databases
-    (sql-transaction conn
-                     "DECLARE mycursor CURSOR FOR select * from pg_database"
-                     PGRES_COMMAND_OK)
-
-    ;; FETCH ALL
-    (with-sql-transaction (res conn "FETCH ALL in mycursor" PGRES_TUPLES_OK)
-      (let* ((nfields (PQnfields res)) (ntuples (PQntuples res))
-             (names (make-array nfields)))
-        (format t " + ~D field~:P; ~D ntuple~:P~%" nfields ntuples)
-
-        ;; first, print out the attribute names
-        (dotimes (ii nfields)
-          (format t "~3:D: ~S~%" ii (setf (aref names ii) (PQfname res ii))))
-
-        ;; next, print out the instances
-        (dotimes (ii ntuples)
-          (format t "~%<<~D>>~%" ii)
-          (dotimes (jj nfields (terpri))
-            (format t "~3:D ~15@S = ~S~%"
-                    jj (aref names jj) (PQgetvalue res ii jj))))))
-
-    ;; close the cursor
-    (sql-transaction conn "CLOSE mycursor" PGRES_COMMAND_OK)
-
-    ;; commit the transaction
-    (sql-transaction conn "COMMIT" PGRES_COMMAND_OK)))
-
-;;;
-;;; asynchronous notification interface
-;;;
-;;; populate a database with the following:
-;;; CREATE TABLE TBL1 (i int4);
-;;; CREATE TABLE TBL2 (i int4);
-;;; CREATE RULE r1 AS ON INSERT TO TBL1 DO [INSERT INTO TBL2 values (new.i); NOTIFY TBL2];
-;;;
-;;; *** psql barfs on this:
-;;; *** ERROR:  parser: parse error at or near ""
-;;; *** ERROR:  parser: parse error at or near "]"
-;;;
-;;;  Then start up this program
-;;;  After the program has begun, do
-;;; INSERT INTO TBL1 values (10);
-
-(defun sql-test-2 ()
-  (with-sql-connection (conn)
-    (sql-transaction conn "LISTEN TBL2" PGRES_COMMAND_OK)
-
-    (loop (PQconsumeInput conn)
-      (loop :for notify = (PQnotifies conn)
-        :while notify :do (format t "ASYNC NOTIFY: ~a~%" notify)
-        (break))
-      (sleep 1))))
-
-;;;
-;;; test the binary cursor interface
-;;;
-;;; *** this is not supported by CLISP at the moment:
-;;; *** need to include geo_decls.h
-;;;
-;;; populate a database by doing the following:
-;;;
-;;;       CREATE TABLE test1 (i int4, d float4, p polygon);
-;;;
-;;;       INSERT INTO test1 values (1, 3.567, '(3.0, 4.0, 1.0, 2.0)'::polygon);
-;;;
-;;;       INSERT INTO test1 values (2, 89.05, '(4.0, 3.0, 2.0, 1.0)'::polygon);
-;;;
-;;;        the expected output is:
-;;;
-;;;       tuple 0: got
-;;;        i = (4 bytes) 1,
-;;;        d = (4 bytes) 3.567000,
-;;;        p = (4 bytes) 2 points         boundbox = (hi=3.000000/4.000000, lo = 1.000000,2.000000)
-;;;       tuple 1: got
-;;;        i = (4 bytes) 2,
-;;;        d = (4 bytes) 89.050003,
-;;;        p = (4 bytes) 2 points         boundbox = (hi=4.000000/3.000000, lo = 2.000000,1.000000)
-;;;
-
-(defun sql-test-3 ()
-  (with-sql-connection (conn)
-    (sql-transaction conn "BEGIN" PGRES_COMMAND_OK)
-
-    (sql-transaction conn
-                     "DECLARE mycursor BINARY CURSOR FOR select * from test1"
-                     PGRES_COMMAND_OK)
-
-    (with-sql-transaction (res conn "FETCH ALL in mycursor" PGRES_TUPLES_OK)
-      (let ((i-fnum (PQfnumber res "i"))
-            (d-fnum (PQfnumber res "d"))
-            (p-fnum (PQfnumber res "p"))
-            (nfields (PQnfields res))
-            (ntuples (PQntuples res)))
-        (format t " + ~d fields; ~d ntuples; i: ~d; d: ~d; p: ~d~%"
-                nfields ntuples i-fnum d-fnum p-fnum)
-        (dotimes (ii 3)
-          (format t "type[~d] = ~d, size[~d] = ~d~%"
-                  ii (PQftype res ii) ii (PQfsize res ii)))
-        (dotimes (ii ntuples)
-          (let ((plen (PQgetlength res ii p-fnum))
-                (ival (PQgetvalue res ii i-fnum))
-                (dval (PQgetvalue res ii d-fnum)))
-            (format t " ++ plen: ~d; ival: ~d; dval: ~f~%" plen ival dval)))))
-
-    (sql-transaction conn "CLOSE mycursor" PGRES_COMMAND_OK)
-    (sql-transaction conn "COMMIT" PGRES_COMMAND_OK)))
-
-)
