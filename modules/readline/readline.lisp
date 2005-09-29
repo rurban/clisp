eeead8a95354400ffc4e1dd09f691bc3d3636fc8
sds <sds@podval.org>
2005-09-29 21:30:29 +0000

    another handful of functions

--- modules/readline/readline.lisp
+++ modules/readline/readline.lisp
@@ -1,4 +1,6 @@
-;;; readline interface
+;;; readline & history interface
+;;; http://cnswww.cns.cwru.edu/~chet/readline/readline.html
+;;; http://cnswww.cns.cwru.edu/~chet/readline/history.html
 ;;;
 ;;; Copyright (C) 2005 by Sam Steingold
 ;;; This is Free Software, covered by the GNU GPL (v2)
@@ -18,6 +20,9 @@
 (default-foreign-language :stdc)
 
 (c-lines "#include <stdio.h>~%")
+
+;;; ------ readline ------
+
 (c-lines "#include <readline/readline.h>~%")
 
 (def-c-type readline-command (c-function (:arguments (rep int) (char int))))
@@ -37,6 +42,8 @@
   (:arguments (name c-string) (callback readline-command) (key int))
   (:return-type int))
 
+;;; Binding Keys
+
 (def-call-out bind-key (:name "rl_bind_key")
   (:arguments (key int) (command readline-command))
   (:return-type int))
@@ -49,9 +56,18 @@
   (:arguments (variable c-string) (value c-string))
   (:return-type int))
 
+;;; Associating Function Names and Bindings
+
 (def-call-out named-function (:name "rl_named_function")
   (:arguments (name c-string))
   (:return-type readline-command))
+
+;;; Redisplay
+
+(def-call-out redisplay (:name "rl_redisplay")
+  (:arguments) (:return-type int))
+
+;;; variables
 
 (def-c-var library-version (:name "rl_library_version") (:type c-string))
 (def-c-var readline-version (:name "rl_readline_version") (:type int))
@@ -59,7 +75,11 @@
 (def-c-var insert-mode (:name "rl_insert_mode") (:type int))
 (def-c-var readline-name (:name "rl_readline_name") (:type c-string)) ;"CLISP"
 
+;;; ------ history ------
+
 (c-lines "#include <readline/history.h>~%")
+
+;;; History List Management
 
 (def-call-out using-history (:name "using_history")
   (:arguments) (:return-type nil))
@@ -79,7 +99,31 @@
 (def-call-out history-stifled-p (:name "history_is_stifled")
   (:arguments) (:return-type int))
 
-;;; history file
+;;; Information About the History List
+
+(def-call-out where-history (:name "where_history")
+  (:arguments) (:return-type int))
+
+(def-call-out history-total-bytes (:name "history_total_bytes")
+  (:arguments) (:return-type int))
+
+;;; Moving Around the History List
+
+(def-call-out history-set-pos (:name "history_set_pos")
+  (:arguments (pos int)) (:return-type int))
+
+;;; Searching the History List
+
+(def-call-out history-search (:name "history_search")
+  (:arguments (string c-string) (direction int)) (:return-type int))
+
+(def-call-out history-search-prefix (:name "history_search_prefix")
+  (:arguments (string c-string) (direction int)) (:return-type int))
+
+(def-call-out history-search-pos (:name "history_search_pos")
+  (:arguments (string c-string) (direction int) (pos int)) (:return-type int))
+
+;;; Managing the History File
 
 (def-call-out read-history (:name "read_history")
   (:arguments (file c-string)) (:return-type int))
@@ -95,6 +139,8 @@
 
 (def-call-out history-truncate-file (:name "history_truncate_file")
   (:arguments (file c-string) (nlines int)) (:return-type int))
+
+;;; done
 
 (pushnew :readline *features*)
 (provide "readline")
