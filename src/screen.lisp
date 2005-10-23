7549e805db383aabcd0347b9ad1cdac3f6651119
sds <unknown>
2005-10-23 01:22:27 +0000

    (*new-window*, read-keyboard-char): removed undocumented and unused symbols

--- src/screen.lisp
+++ src/screen.lisp
@@ -5,47 +5,20 @@
 ;; thus we have to use DEFPACKAGE here just in case
 
 (defpackage "SCREEN"
-  (:documentation "http://clisp.cons.org/impnotes.html#screen")
-  (:use "COMMON-LISP" "EXT"))
+  (:documentation "http://clisp.cons.org/impnotes/screen.html")
+  (:use "COMMON-LISP" "EXT")
+  (:export ;; exported functions and macros:
+   #:make-window #:window-size
+   #:window-cursor-position #:set-window-cursor-position
+   #:clear-window #:clear-window-to-eot #:clear-window-to-eol
+   #:delete-window-line #:insert-window-line
+   #:highlight-on #:highlight-off #:window-cursor-on #:window-cursor-off
+   #:with-window #:*window*))
 
 (in-package "SCREEN")
 
-#|
-; Re-Export von importierten Symbolen kann man nicht mit der
-; P I S E R U I - Regel erreichen. Diese Symbole muss man zuerst importieren.
- (in-package "SYSTEM")
- (import '(make-window window-size
-          window-cursor-position set-window-cursor-position
-          clear-window clear-window-to-eot clear-window-to-eol
-          delete-window-line insert-window-line
-          highlight-on highlight-off window-cursor-on window-cursor-off)
-        "SCREEN")
- (in-package "SCREEN")
-|#
-
-(export '(; exported functions and macros:
-          make-window window-size
-          window-cursor-position set-window-cursor-position
-          clear-window clear-window-to-eot clear-window-to-eol
-          delete-window-line insert-window-line
-          highlight-on highlight-off window-cursor-on window-cursor-off
-          with-window *window*
-          read-keyboard-char
-          ; user-settable things:
-          *new-window*
-)        )
-
-(proclaim '(special *window*)) ; ABI
-
-(defun read-keyboard-char (stream)
-  (declare (ignore stream))
-  (read-char *keyboard-input*)
-)
-
-(defconstant *new-window* nil)
+(defvar *window*) ; ABI
 
 (defmacro with-window (&body body)
   `(LET ((*WINDOW* (MAKE-WINDOW)))
-     (UNWIND-PROTECT (PROGN ,@body) (CLOSE *WINDOW*))
-   )
-)
+     (UNWIND-PROTECT (PROGN ,@body) (CLOSE *WINDOW*))))
