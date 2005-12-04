46ad75e4c56ea85c0f518e8269f003c2a30ca9f1
sds <unknown>
2005-12-04 22:24:02 +0000

    clisp 2.36 release

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=2.35
-RELEASE_DATE=2005-08-29         # in "date +%Y-%m-%d" format
+VERSION_NUMBER=2.36
+RELEASE_DATE=2005-12-04         # in "date +%Y-%m-%d" format
