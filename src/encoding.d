dd58297fbfbbf0538a4ba23d90541835f55d027b
sds <unknown>
2005-12-08 18:16:13 +0000

    (base64_range): do not use string for initialization because it allocates an extra element for 0

--- src/encoding.d
+++ src/encoding.d
@@ -47,8 +47,12 @@ global void base64_wcstombs (object encoding, object stream,
 global object base64_range (object encoding, uintL start, uintL end,
                             uintL maxintervals);
 
-static const char base64_table[64] =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static const char base64_table[64] = {
+  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+  'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
+  'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+  'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'
+};
 static const char table_base64[128] = {
   -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,      /*  -1-  9 */
   -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,      /*  10- 19 */
