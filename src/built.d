0fc96f07524c7c829725cd8876abeb0df8077f9a
sds <unknown>
2006-01-20 16:16:52 +0000

    (built_flags): report readline version

--- src/built.d
+++ src/built.d
@@ -1,12 +1,16 @@
 /*
  * Information about the build environment
  * Bruno Haible 2004-2005
- * Sam Steingold 2004-2005
+ * Sam Steingold 2004-2006
  */
 
 #include "lispbibl.c"
 
 #include "cflags.h"
+
+#if defined(GNU_READLINE)
+#include <readline/readline.h>
+#endif
 
 /* Returns a multiline string containing some info about the flags with which
    the executable was built. */
@@ -65,20 +69,25 @@ global object built_flags (void) {
     " TRIVIALMAP_MEMORY"
   #endif
     ;
-  var object result = ascii_to_string(part1);
+  var uintL count = 1;
+  pushSTACK(ascii_to_string(part1));
  #ifdef LIBSIGSEGV_VERSION
   var char libsigsegv_ver[BUFSIZ];
   sprintf(libsigsegv_ver, "\nlibsigsegv %d.%d",
           LIBSIGSEGV_VERSION >> 8, LIBSIGSEGV_VERSION & 0xff);
-  pushSTACK(result); pushSTACK(ascii_to_string(libsigsegv_ver));
-  result = string_concat(2);
+  pushSTACK(ascii_to_string(libsigsegv_ver)); count++;
  #endif
  #ifdef _LIBICONV_VERSION
   var char libiconv_ver[BUFSIZ];
   sprintf(libiconv_ver, "\nlibiconv %d.%d",
           _LIBICONV_VERSION >> 8, _LIBICONV_VERSION & 0xff);
-  pushSTACK(result); pushSTACK(ascii_to_string(libiconv_ver));
-  result = string_concat(2);
+  pushSTACK(ascii_to_string(libiconv_ver)); count++;
  #endif
-  return result;
+ #ifdef RL_VERSION_MAJOR
+  var char libreadline_ver[BUFSIZ];
+  sprintf(libreadline_ver, "\nlibreadline %d.%d",
+          RL_VERSION_MAJOR,RL_VERSION_MINOR);
+  pushSTACK(ascii_to_string(libreadline_ver)); count++;
+ #endif
+  return count == 1 ? popSTACK() : string_concat(count);
 }
