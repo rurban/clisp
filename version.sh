42066a16420e4d95db7be21e80389836b17f2597
sds <sds@podval.org>
2006-07-14 15:11:03 +0000

    clisp 2.39 release

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 # Version number and release date.
-VERSION_NUMBER=2.38
-RELEASE_DATE=2006-01-24         # in "date +%Y-%m-%d" format
+VERSION_NUMBER=2.39
+RELEASE_DATE=2006-07-16         # in "date +%Y-%m-%d" format
