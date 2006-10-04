d74ac803935c42e3ae47dd5d51306d4a9916b6c0
Sam Steingold <sds@gnu.org>
2006-10-04 00:53:35 +0000

    New module libsvm interfaces to <http://www.csie.ntu.edu.tw/~cjlin/libsvm> and makes Support Vector Machines available in CLISP.

---
+++ modules/libsvm/link.sh
@@ -0,0 +1,13 @@
+file_list=''
+mod_list=''
+if test -f libsvm.c; then
+  file_list="$file_list"' libsvm.o'
+  mod_list="$mod_list"' libsvm'
+fi
+make clisp-module \
+     CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" \
+     INCLUDES="$absolute_linkkitdir"
+NEW_FILES="${file_list}"
+NEW_LIBS="${file_list} -lm"
+NEW_MODULES="${mod_list}"
+TO_LOAD='libsvm'
