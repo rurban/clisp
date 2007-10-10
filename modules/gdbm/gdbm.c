96d26759c63b1eab4af7a7007312def5662687aa
sds <sds@podval.org>
2007-10-10 16:17:26 +0000

    closed GDBM is no longer accepted by functions requiring DB access (check_gdbm): accept require_valid_handle arg (GDBM-FILE-SIZE, GDBM-STORE, GDBM-FETCH, GDBM-DELETE, GDBM-FIRSTKEY) (GDBM-NEXTKEY, GDBM-REORGANIZE, GDBM-SYNC, GDBM-EXISTS, GDBM-SETOPT): require an open GDBM

--- modules/gdbm/gdbm.c
+++ modules/gdbm/gdbm.c
@@ -120,39 +120,51 @@ DEFUN(GDBM::GDBM-OPEN, name &key :BLOCKSIZE :READ-WRITE :OPTION :MODE   \
 #define GDBM_SLOT_PATH  2
 #define GDBM_SLOT_KEY   3
 #define GDBM_SLOT_VAL   4
-static GDBM_FILE check_gdbm (object gdbm, gdbm_data_t *key, gdbm_data_t *val)
+static GDBM_FILE check_gdbm (object gdbm, gdbm_data_t *key, gdbm_data_t *val,
+                             bool require_valid_handle)
 {
+  object fp;
   gdbm = check_classname(gdbm, `GDBM::GDBM`);
   if (key && *key == GDBM_DATA_NOTYPE)
     *key = posfixnum_to_V(TheStructure(gdbm)->recdata[GDBM_SLOT_KEY]);
   if (val && *val == GDBM_DATA_NOTYPE)
     *val = posfixnum_to_V(TheStructure(gdbm)->recdata[GDBM_SLOT_VAL]);
-  return nullp(TheStructure(gdbm)->recdata[GDBM_SLOT_FILE]) ? NULL
-    : (GDBM_FILE)TheFpointer(TheStructure(gdbm)->recdata[GDBM_SLOT_FILE])->fp_pointer;
+  fp = TheStructure(gdbm)->recdata[GDBM_SLOT_FILE];
+  if (fpointerp(fp)) return (GDBM_FILE)TheFpointer(fp)->fp_pointer;
+  else if (require_valid_handle) {
+    pushSTACK(`GDBM::GDBM-ERROR`);
+    pushSTACK(`:MESSAGE`);
+    pushSTACK(`"open GDBM file required"`);
+    pushSTACK(`:CODE`); pushSTACK(`:CLOSED-FILE`);
+    pushSTACK(`"~S: ~A"`);
+    pushSTACK(TheSubr(subr_self)->name);
+    pushSTACK(STACK_4); /* message */
+    funcall(L(error_of_type), 8);
+    NOTREACHED;
+  } else return NULL;
 }
 
 DEFUN(GDBM:GDBM-DEFAULT-KEY-TYPE, dbf) {
   gdbm_data_t key = GDBM_DATA_NOTYPE;
-  (void)check_gdbm(popSTACK(),&key,NULL);
+  (void)check_gdbm(popSTACK(),&key,NULL,false);
   VALUES1(check_data_type_reverse(key));
 }
 DEFUN(GDBM:GDBM-DEFAULT-VALUE-TYPE, dbf) {
   gdbm_data_t val = GDBM_DATA_NOTYPE;
-  (void)check_gdbm(popSTACK(),NULL,&val);
+  (void)check_gdbm(popSTACK(),NULL,&val,false);
   VALUES1(check_data_type_reverse(val));
 }
 
 #if defined(HAVE_GDBM_CLOSE)
 DEFUN(GDBM:GDBM-CLOSE, dbf)
 {
-  GDBM_FILE dbf = check_gdbm(STACK_0,NULL,NULL);
+  GDBM_FILE dbf = check_gdbm(STACK_0,NULL,NULL,false);
   if (dbf) {
     SYSCALL(gdbm_close(dbf));
     TheStructure(STACK_0)->recdata[GDBM_SLOT_FILE] = NIL;
     VALUES1(T);
-  } else {
+  } else
     VALUES1(NIL);
-  }
   skipSTACK(1);
 }
 #endif  /* HAVE_GDBM_CLOSE */
@@ -160,7 +172,7 @@ DEFUN(GDBM:GDBM-CLOSE, dbf)
 #if defined(HAVE_GDBM_FDESC)
 DEFUN(GDBM:GDBM-FILE-SIZE, dbf)
 {
-  GDBM_FILE dbf = check_gdbm(popSTACK(),NULL,NULL);
+  GDBM_FILE dbf = check_gdbm(popSTACK(),NULL,NULL,true);
   off_t ret;
   begin_system_call();
   ret = handle_length(NIL,gdbm_fdesc(dbf));
@@ -234,20 +246,15 @@ DEFCHECKER(gdbm_store_flag, default=GDBM_REPLACE, prefix=GDBM, REPLACE INSERT)
 #if defined(HAVE_GDBM_STORE)
 DEFUN(GDBM:GDBM-STORE, dbf key content &key FLAG)
 {
-  GDBM_FILE dbf = check_gdbm(STACK_3,NULL,NULL);
-  int flag = gdbm_store_flag(STACK_0);
+  GDBM_FILE dbf = check_gdbm(STACK_3,NULL,NULL,true);
+  int flag = gdbm_store_flag(STACK_0), status;
   object content_obj = STACK_1;
   object key_obj = STACK_2;
   skipSTACK(4);
-
-  if (dbf) {
-    int status;
-    with_datum(key_obj, key,
-               with_datum(content_obj, content,
-                          status = gdbm_store(dbf, key, content, flag)));
-    VALUES_IF(!status);
-  } else
-    VALUES1(NIL);
+  with_datum(key_obj, key,
+             with_datum(content_obj, content,
+                        status = gdbm_store(dbf, key, content, flag)));
+  VALUES_IF(!status);
 }
 #endif  /* HAVE_GDBM_STORE */
 
@@ -306,32 +313,22 @@ static object datum_to_object (datum d, gdbm_data_t data_type) {
 DEFUN(GDBM:GDBM-FETCH, dbf key &key TYPE)
 {
   gdbm_data_t data_type = check_data_type(popSTACK());
-  GDBM_FILE dbf = check_gdbm(STACK_1,NULL,&data_type);
-  object key_obj = popSTACK();
-  skipSTACK(1);                 /* drop dbf */
-
-  if (dbf) {
-    datum res;
-    with_datum(key_obj, key, res = gdbm_fetch(dbf,key));
-    VALUES1(datum_to_object(res,data_type));
-  } else
-    VALUES1(NIL);
+  GDBM_FILE dbf = check_gdbm(STACK_1,NULL,&data_type,true);
+  datum res;
+  with_datum(STACK_0, key, res = gdbm_fetch(dbf,key));
+  VALUES1(datum_to_object(res,data_type));
+  skipSTACK(2);                 /* cleanup */
 }
 #endif  /* HAVE_GDBM_FETCH */
 
 #if defined(HAVE_GDBM_DELETE)
 DEFUN(GDBM:GDBM-DELETE, dbf key)
 {
-  GDBM_FILE dbf = check_gdbm(STACK_1,NULL,NULL);
-  object key_obj = popSTACK();
-  skipSTACK(1);                 /* drop dbf */
-
-  if (dbf) {
-    int status;
-    with_datum(key_obj, key, status = gdbm_delete(dbf,key));
-    VALUES_IF(status != -1);
-  } else
-    VALUES1(NIL);
+  GDBM_FILE dbf = check_gdbm(STACK_1,NULL,NULL,true);
+  int status;
+  with_datum(STACK_0, key, status = gdbm_delete(dbf,key));
+  VALUES_IF(status != -1);
+  skipSTACK(2);                 /* drop dbf */
 }
 #endif  /* HAVE_GDBM_DELETE */
 
@@ -339,14 +336,10 @@ DEFUN(GDBM:GDBM-DELETE, dbf key)
 DEFUN(GDBM:GDBM-FIRSTKEY, dbf &key TYPE)
 {
   gdbm_data_t data_type = check_data_type(popSTACK());
-  GDBM_FILE dbf = check_gdbm(popSTACK(),&data_type,NULL);
-
-  if (dbf) {
-    datum res;
-    SYSCALL(res = gdbm_firstkey(dbf));
-    VALUES1(datum_to_object(res,data_type));
-  } else
-    VALUES1(NIL);
+  GDBM_FILE dbf = check_gdbm(popSTACK(),&data_type,NULL,true);
+  datum res;
+  SYSCALL(res = gdbm_firstkey(dbf));
+  VALUES1(datum_to_object(res,data_type));
 }
 #endif  /* HAVE_GDBM_FIRSTKEY */
 
@@ -354,16 +347,11 @@ DEFUN(GDBM:GDBM-FIRSTKEY, dbf &key TYPE)
 DEFUN(GDBM:GDBM-NEXTKEY, dbf key &key TYPE)
 {
   gdbm_data_t data_type = check_data_type(STACK_0);
-  object key_obj = STACK_1;
-  GDBM_FILE dbf = check_gdbm(STACK_2,&data_type,NULL);
-  skipSTACK(3);                 /* drop dbf */
-
-  if (dbf) {
-    datum res;
-    with_datum(key_obj, key, res = gdbm_nextkey(dbf,key));
-    VALUES1(datum_to_object(res,data_type));
-  } else
-    VALUES1(NIL);
+  GDBM_FILE dbf = check_gdbm(STACK_2,&data_type,NULL,true);
+  datum res;
+  with_datum(STACK_1, key, res = gdbm_nextkey(dbf,key));
+  VALUES1(datum_to_object(res,data_type));
+  skipSTACK(3);                 /* cleanup */
 }
 #endif  /* HAVE_GDBM_NEXTKEY */
 
@@ -371,47 +359,34 @@ DEFUN(GDBM:GDBM-NEXTKEY, dbf key &key TYPE)
     int status;                                 \
     SYSCALL(status = statement);                \
     if (status == -1) error_gdbm(NULL);         \
-    else VALUES1(T);                            \
+    else VALUES0;                               \
   } while(0)
 
 #if defined(HAVE_GDBM_REORGANIZE)
 DEFUN(GDBM:GDBM-REORGANIZE, dbf)
 {
-  GDBM_FILE dbf = check_gdbm(popSTACK(),NULL,NULL);
-
-  if (dbf) {
-    CHECK_RUN(gdbm_reorganize(dbf));
-  } else
-    VALUES1(NIL);
+  GDBM_FILE dbf = check_gdbm(popSTACK(),NULL,NULL,true);
+  CHECK_RUN(gdbm_reorganize(dbf));
 }
 #endif  /* HAVE_GDBM_REORGANIZE */
 
 #if defined(HAVE_GDBM_SYNC)
 DEFUN(GDBM:GDBM-SYNC, dbf)
 {
-  GDBM_FILE dbf = check_gdbm(popSTACK(),NULL,NULL);
-
-  if (dbf) {
-    SYSCALL(gdbm_sync(dbf));
-    VALUES1(T);
-  } else
-    VALUES1(NIL);
+  GDBM_FILE dbf = check_gdbm(popSTACK(),NULL,NULL,true);
+  SYSCALL(gdbm_sync(dbf));
+  VALUES0;
 }
 #endif  /* HAVE_GDBM_SYNC */
 
 #if defined(HAVE_GDBM_EXISTS)
 DEFUN(GDBM:GDBM-EXISTS, dbf key)
 {
-  GDBM_FILE dbf = check_gdbm(STACK_1,NULL,NULL);
-  object key_obj = popSTACK();
-  skipSTACK(1);                 /* drop dbf */
-
-  if (dbf) {
-    int status;
-    with_datum(key_obj, key, status = gdbm_exists(dbf, key));
-    VALUES_IF(status);
-  } else
-    VALUES1(NIL);
+  GDBM_FILE dbf = check_gdbm(STACK_1,NULL,NULL,true);
+  int status;
+  with_datum(STACK_0, key, status = gdbm_exists(dbf, key));
+  VALUES_IF(status);
+  skipSTACK(2);                 /* cleanup */
 }
 #endif  /* HAVE_GDBM_EXISTS */
 
@@ -420,31 +395,27 @@ DEFCHECKER(gdbm_setopt_option, prefix=GDBM, CACHESIZE FASTMODE SYNCMODE \
 #if defined(HAVE_GDBM_SETOPT)
 DEFUN(GDBM:GDBM-SETOPT, dbf option value)
 {
-  GDBM_FILE dbf = check_gdbm(STACK_2,NULL,NULL);
+  GDBM_FILE dbf = check_gdbm(STACK_2,NULL,NULL,true);
   int option = gdbm_setopt_option(STACK_1);
   object value = STACK_0;
-
-  if (dbf) {
-    int v;
-    switch (option) {
-      case GDBM_CACHESIZE:
-        v = I_to_sint(check_sint(value));
-        goto gdbm_setopt_common;
-      case GDBM_FASTMODE: case GDBM_SYNCMODE:
-      case GDBM_CENTFREE: case GDBM_COALESCEBLKS:
-        v = nullp(value) ? 0 : 1; break;
-      gdbm_setopt_common:
-        CHECK_RUN(gdbm_setopt(dbf, option, &v, sizeof(int)));
-        break;
-      case GDBM_DEFAULT_VALUE_TYPE: v = GDBM_SLOT_VAL; goto gdbm_setopt_slot;
-      case GDBM_DEFAULT_KEY_TYPE: v = GDBM_SLOT_KEY; gdbm_setopt_slot:
-        TheStructure(STACK_2)->recdata[v] = fixnum(check_data_type(STACK_0));
-        VALUES1(T);
-        break;
-      default: NOTREACHED;
-    }
-    skipSTACK(3);
-  } else
-    VALUES1(NIL);
+  int v;
+  switch (option) {
+    case GDBM_CACHESIZE:
+      v = I_to_sint(check_sint(value));
+      goto gdbm_setopt_common;
+    case GDBM_FASTMODE: case GDBM_SYNCMODE:
+    case GDBM_CENTFREE: case GDBM_COALESCEBLKS:
+      v = nullp(value) ? 0 : 1; break;
+    gdbm_setopt_common:
+      CHECK_RUN(gdbm_setopt(dbf, option, &v, sizeof(int)));
+      break;
+    case GDBM_DEFAULT_VALUE_TYPE: v = GDBM_SLOT_VAL; goto gdbm_setopt_slot;
+    case GDBM_DEFAULT_KEY_TYPE: v = GDBM_SLOT_KEY; gdbm_setopt_slot:
+      TheStructure(STACK_2)->recdata[v] = fixnum(check_data_type(STACK_0));
+      VALUES0;
+      break;
+    default: NOTREACHED;
+  }
+  skipSTACK(3);
 }
 #endif  /* HAVE_GDBM_SETOPT */
