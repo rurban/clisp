250f7df292ba85aa1a7e0574f5f520564d86555f
sds <sds@podval.org>
2007-10-29 14:44:37 +0000

    extend to fully support the makemake equivalent  sed -e 's/, *)/,_EMA_)/g' -e 's/, *,/,_EMA_,/g' Reported by Vadim Konovalov <vkonovalov@alcatel-lucent.com>

--- utils/deema.c
+++ utils/deema.c
@@ -1,14 +1,11 @@
-/* C-Programm-Präprozessor-Hilfe:
-   Fügt _EMA_ in leere Macro-Argumente ein.
-   Äquivalent zu  sed -e 's/,)/,_EMA_)/g'
+/* Work around CPP deficiencies: put _EMA_ instead of Empty Macro Arguments.
+   Equivalent to sed -e 's/, *)/,_EMA_)/g' -e 's/, *,/,_EMA_,/g'
    Bruno Haible 29.12.1993
+   Sam Steingold 2007-10-29
 */
 
 #include <stdio.h>
-
-#ifdef __cplusplus
-extern "C" void exit(int);
-#endif
+#include <stdlib.h>
 
 int main ()
 {
@@ -16,12 +13,13 @@ int main ()
   int c1 = -1;
   while (1) {
     c = getchar(); if (c==EOF) break;
-    if ((c == ')') && (c1 == ','))
+    if ((c1 == ',') && ((c == ')') || (c == ',')))
       fputs("_EMA_",stdout);
     putchar(c);
-    c1 = c;
+    if (c != ' ')
+      c1 = c;
   }
   if (ferror(stdin) || ferror(stdout) || fclose(stdout)) { exit(1); }
-  exit(0);
+  return 0;
 }
 
