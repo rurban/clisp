2064b471ec0f3bf09f3ebabaa6346940972b007e
sds <unknown>
2007-11-15 04:42:33 +0000

    use \uNNNN to output Unicode characters; use "Courier New" (fixed width) font to preserve formatting use bold for the header do not use regexp

--- win32msvc/nsis/text_to_rtf.lisp
+++ win32msvc/nsis/text_to_rtf.lisp
@@ -1,32 +1,35 @@
-(with-open-file (infile "COPYRIGHT" :direction :input :if-does-not-exist :error)
-  (with-open-file (outfile "COPYRIGHT.rtf" :direction :output
-                           :if-exists :overwrite :if-does-not-exist :create)
-    (write-string
-     (do ((output (format nil "{\\rtf1\\ansi\\ansicpg1252\\deff0{\\fonttbl{\\f0\\fswiss\\fprq2\\fcharset0 Arial;}{\\f1\\fswiss\\fcharset0 Arial;}}~%\\viewkind4\\uc1\\pard\\lang1033\\f0\\fs20                                  %NAME% %VERSION%")
-                  (if (and line
-                           (not (regexp:match "-\*- coding: utf-8 -\*-" line)))
-                      (format nil "~A~A\\par~%" output line)
-                      output))
-          (line "" (read-line infile nil)))
-         ((not line) (format nil "~A~%}~%" output))
-       (do ((match-oe (if line (regexp:match "ö" line))
-		      (if line (regexp:match "ö" line)))
-	    (match-ue (if line (regexp:match "ü" line))
-		      (if line (regexp:match "ü" line))))
-	   ((not (or match-oe match-ue)))
-	 (setf line (cond
-		      (match-oe
-		       (format nil "~A~A~A"
-                               (subseq line 0 (regexp:match-start match-oe))
-                               "\\f1\\'f6\\f0 "
-                               (subseq line (regexp:match-end match-oe)
-                                       (length line))))
-		      (match-ue
-		       (format nil "~A~A~A"
-                               (subseq line 0 (regexp:match-start match-ue))
-                               "\\f1\\'f6\\f0 "
-                               (subseq line (regexp:match-end match-ue)
-				       (length line))))
-		      (t line)))))
-     outfile)))
-    
+;; convert plain text to RTF
+;; Copyright (C) Elliott Slaughter 2007
+;; Copyright (C) Sam Steingold 2007
+
+(defun write-string-rtf (string out)
+  ;; unicode chars are represented as \u<decimal>.
+  ;; alas, CHARSET:JAVA uses \u<hexadecimal> so it is no help.
+  ;; non-unicode RTF viewers will display "?"
+  (loop :with start = 0
+    :for pos = (position 128 string :start start :key #'char-code :test #'<=)
+    :if pos :do
+    (write-string string out :start start :end pos)
+    (format out "\\u~D?" (char-code (char string pos)))
+    (setq start (1+ pos))
+    :else :do (write-string string out :start start)
+    (loop-finish))
+  (write-line "\\line" out))
+
+(defun text2rtf (input output)
+  (with-open-file (in input :direction :input :external-format charset:utf-8)
+    (format t "~&Reading ~:D byte~:P from [~A]~%"
+            (file-length in) (truename in))
+    (with-open-file (out (or output (make-pathname :type "rtf" :defaults in))
+                         :direction :output :if-exists :supersede
+                         :external-format charset:ascii)
+      (write-line "{\\rtf1\\ansi\\ansicpg1252\\deff0{\\fonttbl{\\f0\\fswiss\\fprq2\\fcharset0 Courier New;}{\\f1\\fswiss\\fcharset0 Courier New;}}\\line
+\\viewkind4\\uc1\\pard\\lang1033\\f0\\fs18                               {\\b %NAME% %VERSION%}\\line" out)
+      (loop :for line = (read-line in nil nil) :while line
+        :unless (search "-*-" line) ; strip emace cookies
+        :do (write-string-rtf line out))
+      (write-line "}" out)
+      (format t "~&Wrote ~:D byte~:P to [~A]~%"
+              (file-length out) (truename out)))))
+
+(text2rtf (first *args*) (second *args*))
