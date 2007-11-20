80d8429b748de484f5b7c49218c8481df57203a3
sds <unknown>
2007-11-20 14:57:32 +0000

    comment

--- modules/libsvm/link.sh
+++ modules/libsvm/link.sh
@@ -1,6 +1,6 @@
 file_list=''
 mod_list=''
-if test -f libsvm.c; then
+if test -f libsvm.c; then # if we use :library in ffi, no C file is created
   file_list="$file_list"' libsvm.o'
   mod_list="$mod_list"' libsvm'
 fi
