3d6348920c0027cb3db579299cdf6abddade37a0
Sam Steingold <sds@gnu.org>
2007-12-13 20:42:34 +0000

    convert comments "# " --> "/**/"

--- src/spvw_ctype.d
+++ src/spvw_ctype.d
@@ -1,17 +1,17 @@
-# Choice of character set, and internationalization.
+/* Choice of character set, and internationalization. */
 
-# ------------------------------ Specification --------------------------------
+/* -------------------------- Specification ---------------------------- */
 
 local void init_ctype (void);
 
-# ------------------------------ Implementation -------------------------------
+/* -------------------------- Implementation --------------------------- */
 
 local void init_ctype() {
-  # When you call setlocale(LC_CTYPE,""), it examines the environment
-  # variables:
-  # 1. environment variable LC_ALL - an override for all LC_* variables,
-  # 2. environment variable LC_CTYPE,
-  # 3. environment variable LANG - a default for all LC_* variables.
+  /* When you call setlocale(LC_CTYPE,""), it examines the environment
+   variables:
+   1. environment variable LC_ALL - an override for all LC_* variables,
+   2. environment variable LC_CTYPE,
+   3. environment variable LANG - a default for all LC_* variables. */
   var const char * locale;
   locale = getenv("CLISP_LC_CTYPE");
   if (!locale || !*locale) {
@@ -22,17 +22,17 @@ local void init_ctype() {
         locale = getenv("LANG");
     }
   }
-  # Make the <ctype.h> functions 8-bit clean, if the LC_CTYPE environment
-  # variable is set accordingly.
-  # (We don't use these functions directly, but additional modules like
-  # regexp use them and profit from this.)
+  /* Make the <ctype.h> functions 8-bit clean, if the LC_CTYPE environment
+   variable is set accordingly.
+   (We don't use these functions directly, but additional modules like
+   regexp use them and profit from this.) */
   if (locale && *locale)
     setlocale(LC_CTYPE,locale);
 
-  # Also set the other locale facets that may be used by i18n.d.
-  #if HAVE_LC_MESSAGES
+  /* Also set the other locale facets that may be used by i18n.d. */
+ #if HAVE_LC_MESSAGES
   setlocale(LC_MESSAGES,"");
-  #endif
+ #endif
   setlocale(LC_CTYPE,"");
   setlocale(LC_TIME,"");
   setlocale(LC_COLLATE,"");
