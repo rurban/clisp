add044b8b695b399b0006b6a313a1237863b77cb
sds <unknown>
2008-05-23 14:17:56 +0000

    fix bug #[ 1970141 ]: link.sh should respect $(MAKE) makemake.in (base, full): set MAKE when calling clisp-link *link.sh*: use ${MAKE-make} instead of make

--- modules/libsvm/link.sh
+++ modules/libsvm/link.sh
@@ -4,9 +4,9 @@ if test -f libsvm.c; then # if we use :library in ffi, no C file is created
   file_list="$file_list"' libsvm.o'
   mod_list="$mod_list"' libsvm'
 fi
-make clisp-module \
-     CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" \
-     INCLUDES="$absolute_linkkitdir"
+${MAKE-make} clisp-module \
+  CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" \
+  INCLUDES="$absolute_linkkitdir"
 NEW_FILES="${file_list}"
 NEW_LIBS="${file_list} -lm"
 NEW_MODULES="${mod_list}"
