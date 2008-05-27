0b9a548f1dd59b58f11e06806865f4c0c29b8a3e
sds <sds@podval.org>
2008-05-27 21:06:07 +0000

    (unused): undef before #include "lightning.c"

--- src/eval.d
+++ src/eval.d
@@ -8266,6 +8266,7 @@ global maygc void init_cclosures (void) {
 
 #if defined(USE_JITC)
  #if defined(lightning)
+  #undef unused
   #include "lightning.c"
  #else
   #error USE_JITC: what is your JITC flavor?
