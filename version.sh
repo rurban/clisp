2d40ba90282d13b4c656cc0fecc8e16a3c3d042b
sds <unknown>
2008-06-29 03:04:10 +0000

    new version number convention: VERSION_NUMBER is   = MAJOR.MINOR for release,   = MAJOR.MINOR+ for development, and   = MAJOR.MINOR++ for pretest RELEASE_DATE is the date when VERSION_NUMBER was modified. Set to 2.45+/2008-06-28.

--- version.sh
+++ version.sh
@@ -1,3 +1,8 @@
 # Version number and release date.
-VERSION_NUMBER=2.46
-RELEASE_DATE=2008-07-07         # in "date +%F" format
+# VERSION_NUMBER should be
+#  = MAJOR.MINOR for release,
+#  = MAJOR.MINOR+ for development, and
+#  = MAJOR.MINOR++ for pretest
+# RELEASE_DATE should be the date when VERSION_NUMBER was modified.
+VERSION_NUMBER=2.45+
+RELEASE_DATE=2008-06-28         # in "date +%F" format
