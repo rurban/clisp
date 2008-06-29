2bcef3345a5112d2a91cfb14b6dc75dda3f8effe
sds <sds@podval.org>
2008-06-29 20:35:08 +0000

    2.45++: pretest [+ gnulib-imported build-aux-update]

--- version.sh
+++ version.sh
@@ -4,5 +4,5 @@
 #  = MAJOR.MINOR+ for development, and
 #  = MAJOR.MINOR++ for pretest
 # RELEASE_DATE should be the date when VERSION_NUMBER was modified.
-VERSION_NUMBER=2.45+
-RELEASE_DATE=2008-06-28         # in "date +%F" format
+VERSION_NUMBER=2.45++
+RELEASE_DATE=2008-06-29         # in "date +%F" format
