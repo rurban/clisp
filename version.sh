9b4eabb8f3ef7a84eba789d91f78e08dfe922097
sds <sds@podval.org>
2008-06-30 14:30:20 +0000

    comment

--- version.sh
+++ version.sh
@@ -1,8 +1,8 @@
 # Version number and release date.
 # VERSION_NUMBER should be
 #  = MAJOR.MINOR for release,
-#  = MAJOR.MINOR+ for development, and
-#  = MAJOR.MINOR++ for pretest
+#  = MAJOR.MINOR+ for development after MAJOR.MINOR, and
+#  = MAJOR.MINOR++ for pretest (becomes (MINOR+1) on release)
 # RELEASE_DATE should be the date when VERSION_NUMBER was modified.
 VERSION_NUMBER=2.45++
 RELEASE_DATE=2008-06-29         # in "date +%F" format
