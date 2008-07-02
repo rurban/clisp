fb581c895a4e896eafb84cca757aaabeebaccbae
Sam Steingold <sds@gnu.org>
2008-07-02 15:27:18 +0000

    2.46 (2008-07-02)

--- version.sh
+++ version.sh
@@ -4,5 +4,5 @@
 #  = MAJOR.MINOR+ for development after MAJOR.MINOR, and
 #  = MAJOR.MINOR++ for pretest (becomes (MINOR+1) on release)
 # RELEASE_DATE should be the date when VERSION_NUMBER was modified.
-VERSION_NUMBER=2.45++
-RELEASE_DATE=2008-06-29         # in "date +%F" format
+VERSION_NUMBER=2.46
+RELEASE_DATE=2008-07-02         # in "date +%F" format
