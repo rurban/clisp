9103e3daeca081a5e869d71c044d8ade5cbd96a6
sds <unknown>
2008-08-07 21:14:38 +0000

    (fill-stream-flush-buffer): remove the "one-line sexp on its own line" special case, see <http://article.gmane.org/gmane.lisp.clisp.devel/18759>

--- src/fill-out.lisp
+++ src/fill-out.lisp
@@ -61,14 +61,11 @@ T (same indentation as the text, i.e., the same effect as #'IDENTITY).")
       ((t) text-indent)
       (t sexp-indent-value))))
 
-;; SEXP output has 3 cases:
-;;  0. SEXP in-line: needs _no_ indentation
-;;  1. 1-line SEXP on its own line: needs indentation, no embedded newlines
-;;  2. multi-line SEXP in its own block: needs indentation on all lines,
-;;     starts with a newline
+;; SEXP is either a single line, then it is formatted inline as a word,
+;; or it takes several lines, then it is formatted as an indented block
 
 ;; flush the buffer and print a newline (when NEWLINE-P is non-NIL)
-(defun fill-stream-flush-buffer (stream newline-p &aux sexp-case)
+(defun fill-stream-flush-buffer (stream newline-p &aux sexp-block-p)
   (with-slots (target-stream buffer pending-indent current-indent
                pending-space inside-sexp)
       stream
@@ -79,36 +76,29 @@ T (same indentation as the text, i.e., the same effect as #'IDENTITY).")
       (when (plusp (length buffer)) ; something in the buffer to flush
         ;; fill: if the buffer does not fit on the line, TERPRI
         (let ((pos (fill-stream-line-position stream)))
-          (if (and pos (<= (right-margin) pos)) ; does not fit on this line
-            (let ((multiline (find #\newline buffer))) ; only inside sexp
-              (unless multiline (newline))
-              (when inside-sexp ; just finished an S-expression
-                (setq newline-p t sexp-case (if multiline 2 1))))
-            (setq sexp-case 0)))
-        (unless (and newline-p inside-sexp) ; S-expression on its own line(s)
+          (when (and pos (<= (right-margin) pos)) ; does not fit on this line
+            (setq sexp-block-p (find #\newline buffer)) ; only inside sexp
+            (unless sexp-block-p (newline))
+            (when inside-sexp ; just finished an S-expression
+              (setq newline-p (or newline-p sexp-block-p)))))
+        (unless sexp-block-p ; S-expression in a block
           (cond (pending-indent      ; do the indent
                  (sys::write-spaces pending-indent target-stream)
                  (setq pending-indent nil))
                 (pending-space
                  (write-char #\Space target-stream))))
         (setq pending-space nil)
-        (if inside-sexp
-          (case sexp-case
-            (0 (write-char-sequence buffer target-stream))
-            (1 (sys::write-spaces (fill-stream-sexp-indent stream)
-                                  target-stream)
-               (write-char-sequence buffer target-stream))
-            (2 (let ((indent (fill-stream-sexp-indent stream)))
-                 (do* ((beg 0 (1+ end))
-                       (end (position #\Newline buffer)
-                            (position #\Newline buffer :start beg)))
-                      ((null end)
-                       (write-char-sequence buffer target-stream :start beg))
-                   (write-char-sequence buffer target-stream
-                                        :start beg :end end)
-                   (terpri target-stream)
-                   (sys::write-spaces indent target-stream)))))
-            (write-char-sequence buffer target-stream))
+        (if sexp-block-p
+          (do* ((indent (fill-stream-sexp-indent stream))
+                (beg 0 (1+ end))
+                (end (position #\Newline buffer)
+                     (position #\Newline buffer :start beg)))
+               ((null end)
+                (write-char-sequence buffer target-stream :start beg))
+            (write-char-sequence buffer target-stream :start beg :end end)
+            (terpri target-stream)
+            (sys::write-spaces indent target-stream))
+          (write-char-sequence buffer target-stream))
         (setf (fill-pointer buffer) 0))
       (when newline-p (newline)))))
 
