eb298b9b547e13527d2048c4dcc7ab0466216a97
sds <sds@podval.org>
2008-10-29 15:28:13 +0000

    (main): avoid a compilation warning; use ascii_to_string() instead of asciz_to_string() for simplicity

--- src/spvw.d
+++ src/spvw.d
@@ -3754,11 +3754,7 @@ global int main (argc_t argc, char* argv[]) {
      mem file!!! Threads do not survive mem file save/restore - just create
      garbage in it :( */
     /* TODO: give better name :)*/
-    var object thr_name = coerce_imm_ss(asciz_to_string("main thread"
- #ifdef UNICODE
-                                                        ,Symbol_value(S(utf_8))
-#endif
-                                                        ));
+    var object thr_name = coerce_imm_ss(ascii_to_string("main thread"));
     pushSTACK(thr_name);
     pushSTACK(allocate_thread(&STACK_0));
     var object new_cons=allocate_cons();
@@ -3771,7 +3767,7 @@ global int main (argc_t argc, char* argv[]) {
     current_thread()->_lthread=lthr;
     TheThread(lthr)->xth_globals=current_thread();
     TheThread(lthr)->xth_system=xthread_self();
-    popSTACK();
+    skipSTACK(1);
   }
 #endif
   /* if the image was read from the executable, argv1.argv_memfile was
