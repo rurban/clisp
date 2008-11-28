8da37bdbe09f04b9037c867617e660c622db1694
vtz <unknown>
2008-11-28 16:32:30 +0000

    define O(dynamic_8bit_vector), O(dynamic_c_string) as thread-local constant objects

--- src/constobj_tl.d
+++ src/constobj_tl.d
@@ -5,6 +5,12 @@
  > name: object is addressable as TLO(name) */
 
 /* internal variables of the reader: */
-LISPOBJ_TL(token_buff_1)
-LISPOBJ_TL(token_buff_2)
-LISPOBJ_TL(displaced_string)
+LISPOBJ_TL(token_buff_1,".")
+LISPOBJ_TL(token_buff_2,".")
+LISPOBJ_TL(displaced_string,".")
+
+/* for STREAM.D */
+#if defined(SPVW_PURE) || ((((STACK_ADDRESS_RANGE << addr_shift) >> garcol_bit_o) & 1) != 0)
+LISPOBJ_TL(dynamic_8bit_vector,"NIL") /* cache for macro DYNAMIC_8BIT_VECTOR */
+LISPOBJ_TL(dynamic_string,"NIL") /* cache for macro DYNAMIC_STRING */
+#endif
