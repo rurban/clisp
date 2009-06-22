04ac17e6763864a6ca9eace9995962c8327e68a4
sds <unknown>
2009-06-22 15:19:42 +0000

    set change-log-default-name in vc-dir buffers too

--- emacs/misc.el
+++ emacs/misc.el
@@ -6,12 +6,15 @@
      '("clisp" . "*.[cdfh] *.lisp *.tst *.m4 *.in Makefile* *.xml"))
     (add-to-list 'grep-find-ignored-directories "build*")))
 
+(defun clisp-repo-p (dir)
+  (let ((host (vc-cvs-repository-hostname dir)))
+    (and host (string-match "^clisp\\." host))))
+
 (defun clisp-set-change-log-default-name ()
   "Set `change-log-default-name' appropriately."
   (when (eq (vc-backend buffer-file-name) 'CVS)
-    (let* ((dir (file-name-directory buffer-file-name))
-           (host (vc-cvs-repository-hostname dir)))
-      (when (and host (string-match "^clisp\\." host))
+    (let ((dir (file-name-directory buffer-file-name)))
+      (when (clisp-repo-p dir)
         (let ((cl (expand-file-name "ChangeLog" dir)))
           (set (make-local-variable 'change-log-default-name)
                (cond ((file-exists-p cl) cl)
@@ -24,3 +27,11 @@
 
 ;; append: must come after vc-find-file-hook
 (add-hook 'find-file-hooks 'clisp-set-change-log-default-name t)
+
+(defun clisp-set-change-log-vc-dir ()
+  (when (and (eq vc-dir-backend 'CVS)
+             (clisp-repo-p default-directory))
+    (set (make-local-variable 'change-log-default-name)
+         (expand-file-name "src/ChangeLog" default-directory))))
+
+(add-hook 'vc-dir-mode-hook 'clisp-set-change-log-vc-dir)
