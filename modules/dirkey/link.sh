c0f9f48c20dd6e15aedb9edd1a35d5958ef4aca7
Sam Steingold <sds@gnu.org>
2009-08-10 19:24:40 +0000

    simplified

--- modules/dirkey/link.sh
+++ modules/dirkey/link.sh
@@ -1,9 +1,8 @@
-files='dirkey.o'
 ${MAKE-make} clisp-module \
   CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" \
   INCLUDES="$absolute_linkkitdir"
-NEW_FILES="${files}"
-NEW_LIBS="${files}"
+NEW_FILES="dirkey.o"
+NEW_LIBS="${NEW_FILES}"
 NEW_MODULES='dirkey'
 TO_LOAD='dirkey1'
 TO_PRELOAD="preload.lisp"
