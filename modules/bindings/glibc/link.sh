549c0366aac9012aa3aad57d8d814d06f049cb39
sds <unknown>
2009-09-10 21:26:26 +0000

    pass CLISP="${CLISP}" to make

--- modules/bindings/glibc/link.sh
+++ modules/bindings/glibc/link.sh
@@ -1,6 +1,6 @@
 ${MAKE-make} clisp-module \
   CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" \
-  CLISP_LINKKIT="$absolute_linkkitdir"
+  CLISP_LINKKIT="$absolute_linkkitdir" CLISP="${CLISP}"
 NEW_FILES="linux.o"
 NEW_LIBS="${NEW_FILES} -lm"
 NEW_MODULES="linux"
