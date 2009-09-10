928d133aba1e448c08109c95e95dba441b203d03
Sam Steingold <sds@gnu.org>
2009-09-10 21:41:45 +0000

    (queens): add a prototype to pacify gcc

--- modules/queens/queens.c
+++ modules/queens/queens.c
@@ -4,7 +4,8 @@
 /* dynamic data structures are not needed for such a simple problem */
 #define nmax 100
 
-int queens (int n)                /* function definition in ISO/ANSI C style */
+int queens (int);
+int queens (int n)
 { /* Compute the solutions of the n-queens problem. Assume n>0, n<=nmax.
      We look for a function D:{1,...,n} -> {1,...,n} such that
      D, D+id, D-id are injective. We use backtracking on D(1),...,D(n).
@@ -19,6 +20,7 @@ int queens (int n)                /* function definition in ISO/ANSI C style */
   { int i; for (i=1; i<=n; i++) { freetab1[i] = 0; } }
   { int i; for (i=2; i<=2*n; i++) { freetab2[i] = 0; } }
   { int i; for (i=-(n-1); i<n; i++) { freetab3[i] = 0; } }
+  printf("n=%d\n",n);
  {int counter = 0;
   int i = 0; /* recursion depth */
   int* Dptr = &dtab[0]; /* points to next free D(i) */
