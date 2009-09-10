62793e8f74f259f5f314759d8a9312db62481323
sds <sds@podval.org>
2009-09-10 21:26:26 +0000

    pass CLISP="${CLISP}" to make

--- modules/netica/link.sh
+++ modules/netica/link.sh
@@ -1,6 +1,6 @@
 ${MAKE-make} clisp-module \
   CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" \
-  CLISP_LINKKIT="$absolute_linkkitdir"
+  CLISP_LINKKIT="$absolute_linkkitdir" CLISP="${CLISP}"
 NEW_FILES="netica.o"
 # netica 3.25 requires g++
 NEW_LIBS="${NEW_FILES} -lnetica -L${NETICA_C_API}/lib -lstdc++ -lpthread"
