8f3151df0d347a8b1ed98680189e9c5ed3455726
sds <sds@podval.org>
2009-12-03 17:26:06 +0000

    use options instead of positional args

--- modules/rawsock/demos/sniffer.lisp
+++ modules/rawsock/demos/sniffer.lisp
@@ -1,8 +1,29 @@
 ;;; Packet sniffer
 ;; run as root like this:
-;; sudo clisp sniffer.lisp inet 100
+;; $ sudo clisp sniffer.lisp
+;; or
+;; $ sudo clisp sniffer.lisp -domain packet -type raw
 
 (require "rawsock")
+
+;; parse command line
+(defun get-arg-ht ()
+  (loop :with ht = (make-hash-table :test 'equal)
+    :for (key val) :on *args* :by #'cddr :do
+    (unless val (error "Odd number of arguments: ~S" *args*))
+    (unless (char= #\- (char key 0)) (error "Non-option argument: ~S" key))
+    (let ((v (gethash key ht)))
+      (when v (error "Option ~S given more than once: ~S and ~S" key v val)))
+    (setf (gethash key ht) val)
+    :finally (return ht)))
+
+(defun get-opt (ht opt default pack)
+  (let ((val (gethash opt ht)))
+    (if val
+        (or (parse-integer val :junk-allowed t)
+            (and pack (find-symbol (string-upcase val) pack))
+            (error "Invalid ~S: ~S" opt val))
+        default)))
 
 (defvar *buffer-len* 1518)
 
@@ -25,27 +46,20 @@
          :do (format t "~c" (if (= c 0) #\space (code-char c )))))
       (t (prin1 (rawsock:sockaddr-data device))))))
 
-(defun my-open-socket (domain)
-  (cond ((string= domain "inet")
-         (rawsock:socket :inet :packet #x300))
-        ((string= domain "packet")
-         (rawsock:socket :packet :raw #x300))
-        (t (error "invalid socket domain ~S" domain))))
-
-(defun sniff (domain repeat)
-  (let ((socket (my-open-socket domain)))
+(defun sniff (args)
+  (let ((socket (rawsock:socket
+                 (get-opt args "-domain" :INET "KEYWORD")
+                 (get-opt args "-type" :PACKET "KEYWORD")
+                 (get-opt args "-protocol" #x300 "KEYWORD"))))
     (unwind-protect
          (loop
            :with buffer = (make-array *buffer-len* :fill-pointer 0
                                       :element-type '(unsigned-byte 8))
            :and device = (rawsock:make-sockaddr :UNSPEC)
-           :repeat repeat :do
+           :repeat (get-opt args "-repeat" 10 nil) :do
            (my-rcvfrom socket buffer device)
            (print-sockaddr device)
            (print-buffer buffer))
       (rawsock:sock-close socket))))
 
-(unless (= 2 (length *args*))
-  (error "Expected: ({inet|packet} repeat-count), got ~S" *args*))
-
-(sniff (pop *args*) (parse-integer (pop *args*)))
+(sniff (get-arg-ht))
