43d793afa019c05fa0d9b10d267b5f9afdad7688
Sam Steingold <sds@gnu.org>
2010-06-03 20:51:41 +0000

    init the PARI internal data structures properly, enabling nice error messages to the screen * modules/pari/configure.in: do not check for killallfiles, freeall; check for pari_close * modules/pari/cpari.c (clispErrDie): revert to trivial error message (clisp_exception_handler): remove (init_for_clisp): pass INIT_DFTm to pari_init_opts; do not set GP_DATA (done by pari_init_opts) and default_exception_handler (fini_for_clisp): remove * modules/pari/pari.lisp (pari-fini): call pari_close(), also call pari_close() in :FINI

--- modules/pari/cpari.c
+++ modules/pari/cpari.c
@@ -50,28 +50,16 @@ void clispErrFlush(void) {
 }
 void clispErrDie(void);
 void clispErrDie(void) {
-  if (global_err_data) {
-    pushSTACK(ascii_to_string((char*)global_err_data));
-    error(error_condition,GETTEXT("PARI error: ~S"));
-  } else error(error_condition,GETTEXT("PARI error"));
-}
-
-int clisp_exception_handler (long numerr);
-int clisp_exception_handler (long numerr) {
-  if (errmessage[numerr] != NULL && global_err_data == NULL)
-    global_err_data = (void*)errmessage[numerr];
-  return 0;                     /* continue error processing */
+  error(error_condition,GETTEXT("PARI error"));
 }
 
 PariOUT clispOut = {clispPutc, clispPuts, clispFlush, NULL};
 PariOUT clispErr = {clispErrPutc, clispErrPuts, clispErrFlush, clispErrDie};
 
-extern int (*default_exception_handler)(long);
-
 void init_for_clisp (long parisize, long maxprime)
 {
 #if defined(HAVE_PARI_INIT_OPTS)
-  pari_init_opts(parisize,maxprime,0);
+  pari_init_opts(parisize,maxprime,INIT_DFTm);
 #elif defined(HAVE_PARI_INIT)
   pari_init(parisize,maxprime);
 #else
@@ -79,16 +67,5 @@ void init_for_clisp (long parisize, long maxprime)
 #endif
   pari_outfile = stdout; errfile = stderr; logfile = NULL; infile = stdin;
   pariOut = &clispOut; pariErr = &clispErr;
-  GP_DATA = default_gp_data();
-  default_exception_handler = &clisp_exception_handler;
 }
 
-void fini_for_clisp (int leaving)
-{
-#if defined(HAVE_FREEALL)
-  freeall();
-#endif
-#if defined(HAVE_KILLALLFILES)
-  killallfiles(leaving);
-#endif
-}
