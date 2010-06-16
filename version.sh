a111a969f8c6ad3b9f69c822351ee2de96445dd0
sds <unknown>
2010-06-16 15:40:34 +0000

    2.48++ (2010-06-16)

--- version.sh
+++ version.sh
@@ -4,5 +4,5 @@
 #  = MAJOR.MINOR+ for development after MAJOR.MINOR, and
 #  = MAJOR.MINOR++ for pretest (becomes (MINOR+1) on release)
 # RELEASE_DATE should be the date when VERSION_NUMBER was modified.
-VERSION_NUMBER=2.48+
-RELEASE_DATE=2009-08-24         # in "date +%F" format
+VERSION_NUMBER=2.48++
+RELEASE_DATE=2010-06-16         # in "date +%F" format
