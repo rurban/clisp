764da0e8a31b43b54116e45cf7f06c7175cc6351
sds <unknown>
2010-08-31 18:30:43 +0000

    * src/init.lisp (open-for-load): use MUFFLE-CERRORS to ignore encoding errors in DIRECTORY to avoid the DOS attack of CLISP not starting when HOME contains a file incompatible with *PATHNAME-ENCODING*. This finishes the work started on 2008-07-16. * tests/path.tst: test that LOAD works even when there are files under *LOAD-PATHS* which cannot be parsed by *PATHNAME-ENCODING*

--- src/init.lisp
+++ src/init.lisp
@@ -1580,6 +1580,9 @@
      (declare (ignore type))
      (apply #'warn format-string args)))))
 
+(unless (fboundp 'ext::muffle-cerrors) ; predef for bootstrap
+  (sys::%putd 'ext::muffle-cerrors #'identity))
+
 (defun open-for-load (filename extra-file-types external-format
                       &aux stream (present-files t) obj path bad-file)
  (block open-for-load
@@ -1649,11 +1652,12 @@
         ;; File with precisely this name not present OR bad
         ;; Search among the files the most recent one
         ;; with the same name and the Extensions "LISP", "FAS":
-        (setq present-files (search-file filename
-                                         (append extra-file-types
-                                                 *compiled-file-types*
-                                                 *source-file-types*)
-                                         nil)))
+        (setq present-files
+              (ext::muffle-cerrors ; ignore encoding errors in DIRECTORY
+               (search-file filename (append extra-file-types
+                                             *compiled-file-types*
+                                             *source-file-types*)
+                            nil))))
       (if present-files
         ;; proceed with the next present file
         (setq path (car present-files) present-files (cdr present-files)
