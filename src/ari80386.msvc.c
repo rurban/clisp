da9469df02efe3a1ab9c058bcf9daee226a0f774
Bruno Haible <bruno@clisp.org>
2017-06-15 13:06:09 +0200

    Regenerated.

--- src/ari80386.msvc.c
+++ src/ari80386.msvc.c
@@ -1100,6 +1100,10 @@ L(dclu2:)   INSN2(mov,l	,R(edx),R(eax))
             ret
 FUNEND()
 
+#if defined __linux__ || defined __FreeBSD__ || defined __FreeBSD_kernel__ || defined __DragonFly__
+            .section .note.GNU-stack,"",@progbits
+#endif
+
 #endif
 
 
--- src/sp80386.msvc.c
+++ src/sp80386.msvc.c
@@ -32,4 +32,8 @@ FUNBEGIN(getSP)
         ret
 FUNEND()
 
+#if defined __linux__ || defined __FreeBSD__ || defined __FreeBSD_kernel__ || defined __DragonFly__
+        .section .note.GNU-stack,"",@progbits
+#endif
+
 
