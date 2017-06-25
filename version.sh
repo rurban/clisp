303d11a45df2b4ceca9245c60b430e1a33cbe944
Bruno Haible <bruno@clisp.org>
2017-06-25 11:25:08 +0200

    Prepare a beta release for translators.

--- version.sh
+++ version.sh
@@ -4,5 +4,5 @@
 #  = MAJOR.MINOR+ for development after MAJOR.MINOR, and
 #  = MAJOR.MINOR++ for pretest (becomes (MINOR+1) on release)
 # RELEASE_DATE should be the date when VERSION_NUMBER was modified.
-VERSION_NUMBER=2.49.50+
-RELEASE_DATE=2017-03-30         # in "date +%F" format
+VERSION_NUMBER=2.49.60
+RELEASE_DATE=2017-06-25         # in "date +%F" format
