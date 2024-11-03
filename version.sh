afa64df1c2bd92175159e8065117ec7286c65522
Bruno Haible <bruno@clisp.org>
2024-11-03 09:59:28 +0100

    Bump version number.
    * version.sh: Bump version number to 2.49.95+.
    * version.h, src/configure: Regenerated.

--- version.sh
+++ version.sh
@@ -4,5 +4,5 @@
 #  = MAJOR.MINOR+ for development after MAJOR.MINOR, and
 #  = MAJOR.MINOR++ for pretest (becomes (MINOR+1) on release)
 # RELEASE_DATE should be the date when VERSION_NUMBER was modified.
-VERSION_NUMBER=2.49.93+
-RELEASE_DATE=2024-07-04         # in "date +%F" format
+VERSION_NUMBER=2.49.95+
+RELEASE_DATE=2024-11-03         # in "date +%F" format
